c899b6860815b67afc93197718bd2f2358bd1549
Praveen Garg <praveen@scaledb.com>
2017-05-19 02:01:02 +0530

    setup.py fixes; moving to the correct place

---
+++ src/python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='FogLAMP',
+    version='0.1',
+    description='FogLAMP',
+    url='http://github.com/foglamp/FogLAMP',
+    author='DB Software Inc.',
+    author_email='info@scaledb.com',
+    license='MIT',
+    packages=['foglamp'],
+    zip_safe=False
+)

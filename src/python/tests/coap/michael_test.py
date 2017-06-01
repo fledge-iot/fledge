a763234b7d0a95d5eb80a53aca72c16964b8cbd3
MichaelFitz <michael@scaledb.com>
2017-06-01 12:12:41 +1000

    test 6

--- src/python/tests/coap/michael_test.py
+++ src/python/tests/coap/michael_test.py
@@ -15,7 +15,7 @@ from aiocoap import *
 # ~/Development/FogLAMP/src/python$ ./build.sh --run
 #
 # testing commit.
-#test 5
+#test 6
 
 async def main():
     protocol = await Context.create_client_context()

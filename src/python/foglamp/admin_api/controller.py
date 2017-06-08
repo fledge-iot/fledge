03420837591d084c0b5a04fa28a51c0f43afacc4
Terris Linenbach <terris@scaledb.com>
2017-06-07 21:30:41 -0700

    Change a comment

--- src/python/foglamp/admin_api/controller.py
+++ src/python/foglamp/admin_api/controller.py
@@ -4,9 +4,8 @@ import asyncio
 
 def start():
     """Create a http server for REST listening on port 8080"""
-    # TODO Read port from config (but might use nginx or gunicorn in production)
+    # TODO Read port from config (but might use nginx in production especially for https)
 
     loop = asyncio.get_event_loop()
     f = loop.create_server(build_app().make_handler(), '0.0.0.0', 8080)
     loop.create_task(f)
-

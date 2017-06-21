bc4f67882897acf34688fab0b4c3ef5d5c2c757f
Ori Shadmon <oshadmon@gmail.com>
2017-06-21 15:11:22 -0700

    updated comments

--- src/python/foglamp/data_purge/sqlalchemy_purge.py
+++ src/python/foglamp/data_purge/sqlalchemy_purge.py
@@ -22,6 +22,14 @@ conn = engine.connect()
 open('logs.db','w').close()
 
 def conver_timestamp(set_time=None):
+    """
+    Convert the information provided by config.yaml, to an actual timedelta format
+    Args:
+        set_time: 
+
+    Returns:
+
+    """
     time_dict={}
     tmp=0
     for value in set_time.split(" "):
@@ -47,29 +55,49 @@ def conver_timestamp(set_time=None):
     return sec+min+hr+day
 
 def purge():
+    """
+        Actual Purge process
+    Returns:
+        amount of time to wait until next excution (based on config.yaml)
+    """
+    # Reald config (yaml File)
     with open('config.yaml','r') as conf:
         config = yaml.load(conf)
     set_time=config['age']
+
+    # Prepare Query stmts
     timestamp = datetime.datetime.strftime(datetime.datetime.now() - conver_timestamp(set_time=set_time),'%Y-%m-%d %H:%M:%S')
     delete = t1.delete().where(t1.c.ts <= timestamp)
     delete = delete.compile(compile_kwargs={"literal_binds": True})
+    # Rows count up to timestamp
     count1 = t1.count().where(t1.c.ts <= timestamp)
     count1 = count1.compile(compile_kwargs={"literal_binds": True})
+    # Row count after TIMESTAMP
     count2 = t1.count().where(t1.c.ts > timestamp)
     count2 = count2.compile(compile_kwargs={"literal_binds": True})
 
+    # Timestamp of execution
+    timestamp = time.time()
+    timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+    # Call query execution
     lthen=get_count(count1)
     database_manage(delete)
     verify=get_count(count1) # Expect 0 each time
     remainder = get_count(count2)
 
-    timestamp = time.time()
-    timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-
+    # Write to log
     f.write('%s\t%s\t\t%s\t%s\n' % (timestamp,lthen,verify,remainder))
     return config['wait']
 
 def get_count(stmt):
+    """
+    Imitate connection to Postgres and execute COUNT queries 
+    Args:
+        stmt: 
+    Returns:
+        COUNT value
+    """
     try:
         result = conn.execute(stmt)
     except conn.Error as e:
@@ -79,7 +107,11 @@ def get_count(stmt):
         return int(result.fetchall()[0][0])
 
 def database_manage(stmt=""):
-    """Imitate connection to Postgres """
+    """
+    Imitate connection to Postgres and execute INSERT query
+    Args:
+        stmt: 
+    """
     try:
         conn.execute(stmt)
     except conn.Error as e:
@@ -89,6 +121,7 @@ def database_manage(stmt=""):
         conn.execute("commit")
 
 if __name__ == '__main__':
+    """The main, which in part also acts a scheduler"""
     f = open('logs.db', 'a')
     f.write('%s\t\t%s\t%s\t%s\n' % ('TIMESTAMP','Total Removed', 'Verify','Reminder'))
     f.close()

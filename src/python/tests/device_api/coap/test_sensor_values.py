e6f3772e2cc5e65813368ad9bd5d5b86b9ff2d06
Terris Linenbach <terris@scaledb.com>
2017-06-27 16:21:03 -0700

    Use pytest-asyncio and refactor

--- src/python/tests/device_api/coap/test_sensor_values.py
+++ src/python/tests/device_api/coap/test_sensor_values.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 
+# FOGLAMP_BEGIN
+# See: http://foglamp.readthedocs.io/
+# FOGLAMP_END
+
 import pytest
 from unittest.mock import MagicMock
 from cbor2 import dumps
-import asyncio
 from aiocoap.numbers.codes import Code as CoAP_CODES
 
 from foglamp.device_api.coap.sensor_values import SensorValues
 
-__author__ = 'Terris Linenbach'
-__version__ = '${VERSION}'
+__author__    = "Terris Linenbach"
+__copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
+__license__   = "Apache 2.0"
+__version__   = "${VERSION}"
 
 # TODO The functions and classes between this line
 # and the line marked END will be replaced by
-# mocks that for the storage layer.
+# mocks for the storage layer.
 #
 # Mock the following code:
 #
@@ -32,13 +37,13 @@ def async_mock(*args, **kwargs):
 
 
 class MockConnection(MagicMock):
-    """Calling execute() on this object returns a coroutine that does nothing
+    """execute() returns a coroutine that does nothing
     """
     execute = async_mock()
 
 
 class AcquireContextManager(MagicMock):
-    """An async context manager that returns a MockConnection object in __aenter__
+    """An async context manager that returns a MockConnection in __aenter__
     """
     async def __aenter__(self):
         return MockConnection()
@@ -48,25 +53,23 @@ class AcquireContextManager(MagicMock):
 
 
 class MockEngine(MagicMock):
-    """aquire() on the object returns a AcquireContextManager object
+    """aquire() returns a AcquireContextManager object
     """
     acquire = AcquireContextManager()
 
 
 class CreateEngineContextManager(MagicMock):
-    """An async context manager that returns a MockEngine object in __aenter__
+    """An async context manager that returns a MockEngine in __aenter__
     """
     async def __aenter__(self):
         return MockEngine()
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         pass
+        
+def _mock_create_engine(mocker):
+    mocker.patch('aiopg.sa.create_engine', return_value=CreateEngineContextManager())
 # END
-
-
-def _run(coroutine):
-    """Block until the provided coroutine has finished"""
-    return asyncio.get_event_loop().run_until_complete(coroutine)
 
 
 class TestSensorValues:
@@ -79,17 +82,17 @@ class TestSensorValues:
         ({'timestamp':'2017-01-01T00:00:00Z'}, CoAP_CODES.BAD_REQUEST),
         ({'timestamp':'2017-01-01T00:00:00Z', 'asset':'test'}, CoAP_CODES.VALID)
     ]
-    """Requests with expected return status code. Each item in this array is
-    a test case.
+    """An array of tuples consisting of (payload, expected status code)
     """
 
     @pytest.mark.parametrize("dict_payload, expected", __requests)
-    def test_payload(self, mocker, dict_payload, expected):
+    @pytest.mark.asyncio
+    async def test_payload(self, mocker, dict_payload, expected):
         """Runs all test cases in the __requests array"""
-        mocker.patch('aiopg.sa.create_engine', return_value=CreateEngineContextManager())
+        _mock_create_engine(mocker)
         sv = SensorValues()
         request = MagicMock()
         request.payload = dumps(dict_payload)
-        return_val = _run(sv.render_post(request))
+        return_val = await sv.render_post(request)
         assert return_val.code == expected
 

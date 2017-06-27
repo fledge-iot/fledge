31dc7829e232131e091adbd41fe1ceaa99275e68
Ori Shadmon <oshadmon@gmail.com>
2017-06-27 14:11:40 -0700

    using json format (which will be stored in DB) instead of pre-existing config/logs files

--- src/python/foglamp/data_purge/sqlalchemy_purge.py
+++ src/python/foglamp/data_purge/sqlalchemy_purge.py
@@ -14,7 +14,6 @@ import os
 import sqlalchemy
 import sys
 import time
-import yaml
 
 
 t1 = sqlalchemy.Table('t1',sqlalchemy.MetaData(),
@@ -26,15 +25,18 @@ conn = engine.connect()
 
 def conver_timestamp(set_time=None):
     """
-    Convert the information provided by config.yaml, to an actual timedelta format
+    Convert "age" in config file to timedelta. If only an integer is specified, then 
+        the code assumes that it is already in minutes (ie age:1 means wait 1 minute) 
     Args:
         set_time: 
-
     Returns:
-
+        timedelta based on age in config file
     """
+    if set_time.isdigit():
+        return datetime.timedelta(minutes=int(set_time))
     time_dict={}
     tmp=0
+
     for value in set_time.split(" "):
         if value.isdigit() is True:
             tmp=int(value)
@@ -57,29 +59,73 @@ def conver_timestamp(set_time=None):
             day = datetime.timedelta(days=time_dict[key])
     return sec+min+hr+day
 
+def convert_sleep(set_time=None):
+    """
+    Convert "wait time" in config file to seconds. If only an integer is specified, then 
+        the code assumes that it is already in seconds (ie wait:1 means wait 1 second) 
+    Args:
+        set_time: 
+
+    Returns:
+        integer (of seconds) based on wait in config 
+    """
+    if set_time.isdigit():
+        return int(set_time)
+    time_dict = {}
+    tmp = 0
+    for value in set_time.split(" "):
+        if value.isdigit() is True:
+            tmp = int(value)
+        else:
+            time_dict[value] = tmp
+    sec = 0
+    min = 0
+    hr = 0
+    dy = 0
+    for key in time_dict:
+        if "sec" in key:
+            sec = time_dict[key]
+        elif "min" in key:
+            min = 60 * time_dict[key]
+        elif ("hour" in key) or ("hr" in key):
+            hr = 60 * 60 * time_dict[key]
+        elif ("day" in key) or ("dy" in key):
+            dy = 60 * 60 * 24 * time_dict[key]
+        else:
+            print("Error: Invalid Value(s) in config file")
+            sys.exit()
+    return sec+min+hr+dy
+
 def purge():
     """
-        Actual Purge process
-    The numbers are roughly calculated because INSERTS are consistently running as other processes 
-    are being executed. 
+    The actual process for purging data. The numbers being retrieved, and stored to file can be considered
+    "close" estimates becuase the database is always having rows inserted into it. The only way to get real
+    values is if data is not being inserted, which is not possible to do given the type of enviornment delt 
+    with. 
+
     Returns:
         amount of time to wait until next execution (based on config.yaml)
     """
 
-    # Reald config (YAML File) - age, enabled, wait, date
-    with open('config.yaml','r') as conf:
-        config = yaml.load(conf)
+    # Reald config (JSON File) - age, enabled, wait, date
+    with open('config.json','r') as conf:
+        config = json.load(conf)
+
     age_timestamp = datetime.datetime.strftime(datetime.datetime.now() - conver_timestamp(set_time=config['age']),'%Y-%m-%d %H:%M:%S')
+
     data = {}
     if os.path.getsize("logs.json") > 0:
-
         with open('logs.json', 'r') as f:
             data=json.load(f)
 
-    if config['enabled'] is True: # If it's enabled
+    if config['enabled'] is True: # Execute purge process if enabled
         purgeStatus = {}
 
+        start = time.time()
+        start = datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S.%s')
+
         # unsentRowsRemoved: How many rows that were not sent to PI were removed
+        unsentRowsRemoved = 0
         if age_timestamp > str(config['pi_date']):
             query = sqlalchemy.select([sqlalchemy.func.count('*')]).select_from(t1).where(t1.c.ts <= age_timestamp).where(t1.c.ts >= str(config['pi_date']))
             unsentRowsRemoved=get_count(query)
@@ -94,7 +140,7 @@ def purge():
 
         # Execute DELETE stmt
         delete = t1.delete().where(t1.c.ts <= age_timestamp)
-        start, end = database_manage(delete)
+        database_manage(delete)
 
         # failedRemovals: How many rows that were expected to get removed weren't removed
         query = sqlalchemy.select([sqlalchemy.func.count('*')]).select_from(t1).where(t1.c.ts <= age_timestamp)
@@ -107,8 +153,10 @@ def purge():
         # rowsRemoved: How many rows were removed
         rowsRemoved = totalCount - rowsRemaining
 
+        end = time.time()
+        end = datetime.datetime.fromtimestamp(end).strftime('%Y-%m-%d %H:%M:%S.%s')
 
-
+        # Set data into table.
         purgeStatus['Start Time'] = start
         purgeStatus['Complete Time'] = end
         purgeStatus['Rows Remaining'] = rowsRemaining
@@ -117,12 +165,12 @@ def purge():
         purgeStatus['Failed Remove'] = failedRemovals
         purgeStatus['Unsent Removed Rows'] = unsentRowsRemoved
 
-        data[start]=purgeStatus
+        data[start]=purgeStatus # Each new set of values is "saved" under its initial time.
         f = open('logs.json', 'w')
         f.write(json.dumps(data))
         f.close()
 
-    return config['wait']
+    return convert_sleep(config['wait'])
 
 def get_count(stmt):
     """

706c5f302567ea1510979b0649aeecab6ba42440
Ori Shadmon <oshadmon@gmail.com>
2017-07-11 15:28:10 -0700

    __init__ should be empty

--- src/python/foglamp/data_purge/__init__.py
+++ src/python/foglamp/data_purge/__init__.py
@@ -1,11 +1,1 @@
-"""
-Date: June 2017
-Description: The __init__.py can be used in one of two ways: 
-        1. Declare all imports that'll be used in directory
-        2. Declare global variables
-Given that the connection, json objects are something that is used throughout, 
-the __init__.py file provides the latter of the two. 
-"""
-import sqlalchemy
 
-# Set variables as needed (specifically `host`)

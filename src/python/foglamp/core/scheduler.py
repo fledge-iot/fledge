45c8c9d42184c30a69b4db73f142aa958c80ee97
Terris Linenbach <terris@scaledb.com>
2017-07-13 22:46:06 -0700

    More interval implementation

--- src/python/foglamp/core/scheduler.py
+++ src/python/foglamp/core/scheduler.py
@@ -83,14 +83,26 @@ class Scheduler(object):
         self.start_time = time.time()
         self.last_check_time = self.start_time
         self.schedule_seconds = 60
-        self.__processes = []
         self.__scheduled_processes = dict()
         self.__schedules = dict()
-
+        self.__schedule_factors = dict()
+        """For interval schedules only
+        
+        Maps schedule id to a "time factor" when process was last executed
+        
+        A "time factor" is the amount of time the scheduler has been running divided by 
+        the schedule's interval time. The division is converted to an integer
+        using floor(). For example, if the interval time is
+        15 minutes and the process has been running for 35 minutes, the
+        time factor is 2.
+        """
+        self.__running_exclusive_schedules = set()
+        """A set of session ids that are running, if they are 'exclusive'"""
+        self.__processes = dict()
         # pylint: disable=line-too-long
-        """Long running processes
+        """Running processes
 
-        A list of
+        A dictionary of
         `Process <https://docs.python.org/3/library/asyncio-subprocess.html#asyncio.asyncio.subprocess.Process>`_
         objects
         """
@@ -106,7 +118,7 @@ class Scheduler(object):
 
         :return True if all processes have stopped
         """
-        # TODO After telling processes to stop, wait for them to stop
+        # TODO: After telling processes to stop, wait for them to stop
         logging.getLogger(__name__).info("Stopping")
         for process in self.__processes:
             try:
@@ -121,7 +133,7 @@ class Scheduler(object):
 
         # TODO what if this fails?
         process = await asyncio.create_subprocess_exec('python3', '-m', 'foglamp.device')
-        self.__processes.append(process)
+        self.__processes[uuid.uuid4()] = process
 
     async def _get_scheduled_processes(self):
         query = sa.select([self.__scheduled_processes_tbl.c.name,
@@ -136,46 +148,71 @@ class Scheduler(object):
     async def _get_schedules(self):
         query = sa.select([self.__schedules_tbl.c.id,
                            self.__schedules_tbl.c.schedule_type,
-                           self.__schedules_tbl.c.schedule_interval,
                            self.__schedules_tbl.c.schedule_time,
+                           self.__schedules_tbl.c.schedule_interval,
                            self.__schedules_tbl.c.exclusive,
                            self.__schedules_tbl.c.process_name])
 
         query.select_from(self.__schedules_tbl)
 
-        Schedule = collections.namedtuple('Schedule', 'type time interval exclusive process_name')
+        ScheduleInfo = collections.namedtuple(
+            'ScheduleInfo',
+            'id type time interval exclusive process_name')
 
         async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 async for row in conn.execute(query):
-                    self.__schedules[row.id] = Schedule(
+                    interval_seconds = None
+                    if row.interval is not None:
+                        t = row.interval
+                        interval_seconds = 3600*t.hour+60*t.minute+t.second
+
+                    self.__schedules[row.id] = ScheduleInfo(
+                                                id=row.id,
                                                 type=row.schedule_type,
                                                 time=row.schedule_time,
-                                                interval=row.schedule_interval,
+                                                interval=interval_seconds,
                                                 exclusive=row.exclusive,
                                                 process_name=row.process_name)
 
-    async def _add_task(self, process_info):
-        row_id = uuid.uuid4()
+    async def _start_process(self, schedule_info):
+        # TODO: What if this fails
+        process = await asyncio.create_subprocess_exec(
+            self.__scheduled_processes[schedule_info.process_name].script)
+
+        task_id = uuid.uuid4()
+        self.__processes[task_id] = process
+
+        if schedule_info.exclusive:
+            self.__running_exclusive_schedules.add(schedule_info.id)
 
         async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 await conn.execute(self.__tasks_tbl.insert().values(
-                                        id=row_id,
-                                        process_name=process_info.name,
+                                        id=task_id,
+                                        process_name=schedule_info.process_name,
                                         state=self._TaskState.running,
                                         start_time=time.time()))
 
-        return row_id
+        asyncio.ensure_future(self._wait_for_task_completion(task_id, schedule_info))
 
-    async def _wait_for_process(self, row_id, process):
-        process.wait()
+    async def _wait_for_task_completion(self, task_id, schedule_info):
+        # TODO: Catch the right exception
+        try:
+            self.__processes[task_id].wait()
+        except Exception:
+            pass
+
+        if schedule_info.exclusive:
+            self.__running_exclusive_schedules.remove(schedule_info.id)
+
+        del self.__processes[task_id]
 
         async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 await conn.execute(
                     self.__tasks_tbl.update().
-                    where(self.__tasks_tbl.c.id == row_id).
+                    where(self.__tasks_tbl.c.id == task_id).
                     values(reason='?',
                            end_time=time.time(),
                            state=self._TaskState.complete))

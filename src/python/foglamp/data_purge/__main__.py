185a841d3d523d1cad33e3c4a5673775f8678f4b
ashwinscale <ashwin@dianomic.com>
2017-07-20 16:44:48 -0700

    FOGL-319 Configuration Manager and purge process integration (#80)

---
+++ src/python/foglamp/data_purge/__main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# FOGLAMP_BEGIN
+# See: http://foglamp.readthedocs.io/
+# FOGLAMP_END
+
+"""Purge process starter"""
+
+from foglamp.data_purge import purge
+
+__author__ = "Terris Linenbach"
+__copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
+__license__ = "Apache 2.0"
+__version__ = "${VERSION}"
+
+
+# TODO: Rename purge_main to main
+purge.purge_main()
+

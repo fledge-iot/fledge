544aa7d1f80d943dce36f5ec2eccdf79bb70b11f
amarendra-dianomic <amarendra@dianomic.com>
2017-07-28 08:48:18 +0530

    Minor fix in statistics demo data

--- src/sql/statistics_demo_data.sql
+++ src/sql/statistics_demo_data.sql
@@ -2,7 +2,7 @@ delete from foglamp.statistics_history;
 
 update statistics set value = 1234, previous_value = 4567;
 
-\set new_ts now()::timestamp(0) - 'interval \'20 minute\''
+\set new_ts now() - 'interval \'20 minute\''
 insert into statistics_history (key, history_ts, value, ts) values ( 'BUFFERED', :new_ts, 10, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'SENT', :new_ts, 11, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSENT', :new_ts, 12, :new_ts );
@@ -10,7 +10,7 @@ insert into statistics_history (key, history_ts, value, ts) values ( 'PURGED', :
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSNPURGED', :new_ts, 14, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'DISCARDED', :new_ts, 15, :new_ts );
 
-\set new_ts now()::timestamp(0) - 'interval \'15 minute\''
+\set new_ts now() - 'interval \'15 minute\''
 insert into statistics_history (key, history_ts, value, ts) values ( 'BUFFERED', :new_ts, 16, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'SENT', :new_ts, 17, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSENT', :new_ts, 18, :new_ts );
@@ -18,7 +18,7 @@ insert into statistics_history (key, history_ts, value, ts) values ( 'PURGED', :
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSNPURGED', :new_ts, 20, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'DISCARDED', :new_ts, 21, :new_ts );
 
-\set new_ts now()::timestamp(0) - 'interval \'10 minute\''
+\set new_ts now() - 'interval \'10 minute\''
 insert into statistics_history (key, history_ts, value, ts) values ( 'BUFFERED', :new_ts, 22, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'SENT', :new_ts, 23, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSENT', :new_ts, 24, :new_ts );
@@ -26,7 +26,7 @@ insert into statistics_history (key, history_ts, value, ts) values ( 'PURGED', :
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSNPURGED', :new_ts, 26, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'DISCARDED', :new_ts, 27, :new_ts );
 
-\set new_ts now()::timestamp(0) - 'interval \'5 minute\''
+\set new_ts now() - 'interval \'5 minute\''
 insert into statistics_history (key, history_ts, value, ts) values ( 'BUFFERED', :new_ts, 28, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'SENT', :new_ts, 29, :new_ts );
 insert into statistics_history (key, history_ts, value, ts) values ( 'UNSENT', :new_ts, 30, :new_ts );

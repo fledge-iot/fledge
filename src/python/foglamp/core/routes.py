9dcbbd935b151d1389ef7a04519f4801fc650af3
Amarendra <aksinha@nerdapplabs.com>
2017-08-08 18:13:24 +0530

    Minor omission, accidently changed, now fixed.

--- src/python/foglamp/core/routes.py
+++ src/python/foglamp/core/routes.py
@@ -61,7 +61,7 @@ def setup(app):
     app.router.add_route('GET', '/foglamp/audit', api_audit.get_audit_entries)
 
     # enable cors support
-    # enable_cors(app)
+    enable_cors(app)
 
     # enable a live debugger (watcher) for requests, see https://github.com/aio-libs/aiohttp-debugtoolbar
     # this will neutralize error middleware

8ae43dc2f16ffce563cef0214719d658bab3ea44
Ashwin Gopalakrishnan <ashwin@dianomic.com>
2017-08-15 18:09:09 -0700

    read all sensor readings

--- src/python/foglamp/device/sensehat.py
+++ src/python/foglamp/device/sensehat.py
@@ -4,17 +4,14 @@
 # See: http://foglamp.readthedocs.io/
 # FOGLAMP_END
 
-"""CoAP handler for sensor readings"""
+"""SenseHat for sensor readings"""
 
 import asyncio
 
-from cbor2 import loads
-from cbor2.decoder import CBORDecodeError
-
-from foglamp import configuration_manager
 from foglamp import logger
 from foglamp.device.ingest import Ingest
 from sense_hat import SenseHat
+from datetime import datetime, timezone
 
 __author__ = "Ashwin Gopalakrishnan"
 __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
@@ -28,20 +25,79 @@ _LOGGER = logger.setup(__name__)
 # pylint: enable=line-too-long
 _CONFIG_CATEGORY_NAME = 'HAT_CONF'
 _CONFIG_CATEGORY_DESCRIPTION = 'Sense Hat Configuration'
-_READINGS_WRITE_FREQUENCY_SECONDS = 5
+_READINGS_WRITE_FREQUENCY_SECONDS = .001
 
 async def start():
+    sense = SenseHat()
+    sense.set_imu_config(True, True, True)
     while True:
         await asyncio.sleep(_READINGS_WRITE_FREQUENCY_SECONDS)
-        payload=\
-        {
-          "timestamp"     : "2017-08-04T06:59:57.503Z",
-          "asset"         : "TI sensorTag/luxometer",
-          "sensor_values" : { "lux" : 49 }
-        }
+
+        # "TI sensorTag/pressure"
         try:
-            await Ingest.add_readings(payload)
-        except (KeyError, TypeError):
-            _LOGGER.exception("Invalid payload: %s", payload)
+            sensor_value_object = {'sensor_values': {'pressure': sense.get_pressure()}}
         except Exception:
-            _LOGGER.exception("Error saving sensor readings: %s", payload)
+            _LOGGER.exception("Unable to read pressure")
+            continue
+        sensor_value_object["asset"] = "TI sensorTag/pressure"
+        sensor_value_object["timestamp"] = "{!s}".format(datetime.now(tz=timezone.utc))
+        try:
+            await Ingest.add_readings(sensor_value_object)
+        except Exception:
+            _LOGGER.exception("Error saving sensor readings: %s", sensor_value_object)
+
+        # "TI sensorTag/humidity"
+        try:
+            sensor_value_object = {'sensor_values': {'humidity': sense.get_humidity(),'temperature': sense.get_temperature()}}
+        except Exception:
+            _LOGGER.exception("Unable to read humdity/temperature")
+            continue
+        sensor_value_object["asset"] = "TI sensorTag/humidity"
+        sensor_value_object["timestamp"] = "{!s}".format(datetime.now(tz=timezone.utc))
+        try:
+            await Ingest.add_readings(sensor_value_object)
+        except Exception:
+            _LOGGER.exception("Error saving sensor readings: %s", sensor_value_object)
+
+        # "TI sensorTag/gyroscope"
+        try:
+            sensor_value_object = {
+                'sensor_values': sense.get_gyroscope_raw()}
+        except Exception:
+            _LOGGER.exception("Unable to read gyroscope ")
+            continue
+        sensor_value_object["asset"] = "TI sensorTag/gyroscope"
+        sensor_value_object["timestamp"] = "{!s}".format(datetime.now(tz=timezone.utc))
+        try:
+            await Ingest.add_readings(sensor_value_object)
+        except Exception:
+            _LOGGER.exception("Error saving sensor readings: %s", sensor_value_object)
+
+        # "TI sensorTag/magnetometer"
+        try:
+            sensor_value_object = {
+                'sensor_values': sense.get_compass_raw()}
+        except Exception:
+            _LOGGER.exception("Unable to read magnetometer ")
+            continue
+        sensor_value_object["asset"] = "TI sensorTag/magnetometer"
+        sensor_value_object["timestamp"] = "{!s}".format(datetime.now(tz=timezone.utc))
+        try:
+            await Ingest.add_readings(sensor_value_object)
+        except Exception:
+            _LOGGER.exception("Error saving sensor readings: %s", sensor_value_object)
+
+        # "TI sensorTag/accelerometer"
+        try:
+            sensor_value_object = {
+                'sensor_values': sense.get_accelerometer_raw()}
+        except Exception:
+            _LOGGER.exception("Unable to read accelerometer ")
+            continue
+        sensor_value_object["asset"] = "TI sensorTag/accelerometer"
+        sensor_value_object["timestamp"] = "{!s}".format(datetime.now(tz=timezone.utc))
+        try:
+            await Ingest.add_readings(sensor_value_object)
+        except Exception:
+            _LOGGER.exception("Error saving sensor readings: %s", sensor_value_object)
+

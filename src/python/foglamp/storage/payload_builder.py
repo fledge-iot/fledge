4ea7c45c25debc92048a7259681beabb975cd52d
Amarendra <amarendra@dianomic.com>
2017-09-29 19:21:47 +0530

    FOGL-521 - fourth pass

--- src/python/foglamp/storage/payload_builder.py
+++ src/python/foglamp/storage/payload_builder.py
@@ -12,40 +12,12 @@ __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
 __license__ = "Apache 2.0"
 __version__ = "${VERSION}"
 
-import json
+import urllib.parse
 from collections import OrderedDict
 from foglamp import logger
 
 _LOGGER = logger.setup(__name__)
 
-
-class Schema(object):
-
-    scheduled_processes = {
-        'name': 'name',
-        'process': 'process'
-    }
-
-    schedules = {
-        'id': 'id',
-        'process_name': 'process_name',
-        'schedule_name': 'schedule_name',
-        'schedule_type': 'schedule_type',
-        'schedule_interval': 'schedule_interval',
-        'schedule_time': 'schedule_time',
-        'schedule_day': 'schedule_day'
-    }
-
-    tasks = {
-        'id': 'id',
-        'process_name': 'process_name',
-        'state': 'state',
-        'start_time': 'start_time',
-        'end_time': 'end_time',
-        'reason': 'reason',
-        'pid': 'pid',
-        'exit_code': 'exit_code'
-    }
 
 class PayloadBuilder(object):
     """ Payload Builder to be used in Python wrapper class for Storage Service
@@ -91,7 +63,7 @@ class PayloadBuilder(object):
         return self
 
     def FROM(self, tbl_name):
-        self.payload.update({'table': tbl_name})
+        self.payload.update({"table": tbl_name})
         return self
 
     def UPDATETABLE(self, tbl_name):
@@ -104,7 +76,7 @@ class PayloadBuilder(object):
         return values
 
     def UPDATE(self, **kwargs):
-        self.payload.update({'values': self.COLS(kwargs)})
+        self.payload.update({"values": self.COLS(kwargs)})
         return self
 
     def INSERT(self, **kwargs):
@@ -120,35 +92,35 @@ class PayloadBuilder(object):
     def WHERE(self, arg):
         condition = {}
         if self.verify_condition(arg):
-            condition.update({'column': arg[0], 'condition': arg[1], 'value': arg[2]})
-            self.payload.update({'where': condition})
+            condition.update({"column": arg[0], "condition": arg[1], "value": arg[2]})
+            self.payload.update({"where": condition})
         return self
 
     def WHERE_AND(self, *args):
         for arg in args:
             condition = {}
             if self.verify_condition(arg):
-                condition.update({'column': arg[0], 'condition': arg[1], 'value': arg[2]})
-                self.payload['where'].update({'and': condition})
+                condition.update({"column": arg[0], "condition": arg[1], "value": arg[2]})
+                self.payload["where"].update({"and": condition})
         return self
 
     def WHERE_OR(self, *args):
         for arg in args:
             if self.verify_condition(arg):
                 condition = {}
-                condition.update({'column': arg[0], 'condition': arg[1], 'value': arg[2]})
-                self.payload['where'].update({'or': condition})
+                condition.update({"column": arg[0], "condition": arg[1], "value": arg[2]})
+                self.payload["where"].update({"or": condition})
         return self
 
     def GROUPBY(self, *args):
-        self.payload.update({'group': ', '.join(args)})
+        self.payload.update({"group": ', '.join(args)})
         return self
 
     def AGGREGATION(self, *args):
         for arg in args:
             aggregation = {}
             if self.verify_aggregation(arg):
-                aggregation.update({'operation': arg[0], 'column': arg[1]})
+                aggregation.update({"operation": arg[0], "column": arg[1]})
                 if 'aggregation' in self.payload:
                     if isinstance(self.payload['aggregation'], list):
                         self.payload['aggregation'].append(aggregation)
@@ -156,7 +128,7 @@ class PayloadBuilder(object):
                         self.payload['aggregation'] = list(self.payload.get('aggregation'))
                         self.payload['aggregation'].append(aggregation)
                 else:
-                    self.payload.update({'aggregation': aggregation})
+                    self.payload.update({"aggregation": aggregation})
         return self
 
     def HAVING(self):
@@ -165,14 +137,14 @@ class PayloadBuilder(object):
 
     def LIMIT(self, arg):
         if isinstance(arg, int):
-            self.payload.update({'limit': arg})
+            self.payload.update({"limit": arg})
         return self
 
     def ORDERBY(self, *args):
         for arg in args:
             sort = {}
             if self.verify_orderby(arg):
-                sort.update({'column': arg[0], 'direction': arg[1]})
+                sort.update({"column": arg[0], "direction": arg[1]})
                 if 'sort' in self.payload:
                     if isinstance(self.payload['sort'], list):
                         self.payload['sort'].append(sort)
@@ -180,14 +152,22 @@ class PayloadBuilder(object):
                         self.payload['sort'] = list(self.payload.get('sort'))
                         self.payload['sort'].append(sort)
                 else:
-                    self.payload.update({'sort': sort})
+                    self.payload.update({"sort": sort})
         return self
 
     def execute(self):
-        return str(self.payload)
+        return dict(self.payload)
 
-if __name__ == '__main__':
-    pb = PayloadBuilder(payload=dict())
+    def execute_query_params(self):
+        where = self.payload['where']
+        query_params = {where['column']: where['value']}
+        for key, value in where.items():
+            if key == 'and':
+                query_params.update({value['column']: value['value']})
+        return urllib.parse.urlencode(query_params)
+
+if __name__ == "__main__":
+    pb = PayloadBuilder(payload=OrderedDict())
     # Select
     sql = pb.\
         SELECT('id', 'type', 'repeat', 'process_name').\
@@ -200,28 +180,38 @@ if __name__ == '__main__':
         ORDERBY(['process_name', 'desc']).\
         AGGREGATION(['count', 'process_name']).\
         execute()
-    print(sql)
+    print(str(sql).replace("'", '"'))
 
-    pb = PayloadBuilder(payload=dict())
+    pb = PayloadBuilder(payload=OrderedDict())
     # Insert
     sql = pb.\
         INSERTINTO('schedules').\
         INSERT(id='test', process_name='sleep', type=3, repeat=45677).\
         execute()
-    print(sql)
+    print(str(sql).replace("'", '"'))
 
-    pb = PayloadBuilder(payload=dict())
+    pb = PayloadBuilder(payload=OrderedDict())
     # Insert
     sql = pb.\
         UPDATETABLE('schedules').\
         UPDATE(id='test', process_name='sleep', type=3, repeat=45677).\
         WHERE(['id', '=', 'test']). \
         execute()
-    print(sql)
+    print(str(sql).replace("'", '"'))
 
+    from foglamp.core.service_registry.service_registry import Service
     from foglamp.storage.storage import Storage
 
-    pb = PayloadBuilder(payload=dict())
-    sql = pb.SELECTALL().FROM('scheduled_processes').execute()
+    Service.Instances.register(name="store", s_type="Storage", address="0.0.0.0", port=8080)
 
-    print(Storage.query_tbl(sql['table']))
+    pb = PayloadBuilder(payload=OrderedDict())
+    sql = pb.WHERE(["key", "=", "COAP_CONF"]).execute()
+    tbl_name = 'configuration'
+    q = str(sql).replace("'", '"')
+    print(Storage().query_tbl_with_payload(tbl_name, q))
+
+    pb = PayloadBuilder(payload=OrderedDict())
+    # sql = pb.WHERE(["key", "=", "COAP_CONF"]).WHERE_AND(["ts", "=", "2017-09-15 12:33:22.619847+05:30"]).execute_query_params()
+    sql = pb.WHERE(["key", "=", "COAP_CONF"]).execute_query_params()
+    tbl_name = 'configuration'
+    print(Storage().query_tbl(tbl_name, sql))

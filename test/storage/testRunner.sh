454bf4824600253a345202c16ed6fdb5b2661638
Mark Riddoch <mark@dianomic.com>
2017-10-04 11:50:58 +0100

    FOGL-197 Added more tests

--- test/storage/testRunner.sh
+++ test/storage/testRunner.sh
@@ -4,36 +4,49 @@ testNum=1
 n_failed=0
 n_passed=0
 n_unchecked=0
-./testSetup.sh > /dev/null
+./testSetup.sh > /dev/null 2>&1
 rm -f failed
 rm -rf results
 mkdir results
-cat testset | while read name method url payload; do
+cat testset | while read name method url payload optional; do
 echo -n "Test $testNum ${name}: "
 if [ "$payload" = "" ] ; then
 	curl -X $method $url -o results/$testNum >/dev/null 2>&1
+	curlstate=$?
 else
 	curl -X $method $url -d@payloads/$payload -o results/$testNum >/dev/null 2>&1
+	curlstate=$?
 fi
-if [ ! -f expected/$testNum ]; then
-	n_unchecked=`expr $n_unchecked + 1`
-	echo Missing expected results for test $testNum - result unchecked
-else
-	cmp -s results/$testNum expected/$testNum
-	if [ $? -ne "0" ]; then
+if [ "$optional" = "" ] ; then
+	if [ ! -f expected/$testNum ]; then
+		n_unchecked=`expr $n_unchecked + 1`
+		echo Missing expected results for test $testNum - result unchecked
+	else
+		cmp -s results/$testNum expected/$testNum
+		if [ $? -ne "0" ]; then
+			echo Failed
+			n_failed=`expr $n_failed + 1`
+			touch failed
+		else
+			echo Passed
+			n_passed=`expr $n_passed + 1`
+		fi
+	fi
+elif [ "$optional" = "checkstate" ] ; then
+	if [ $curlstate -eq 0 ] ; then
+		echo Passed
+		n_passed=`expr $n_passed + 1`
+	else
 		echo Failed
 		n_failed=`expr $n_failed + 1`
 		touch failed
-	else
-		echo Passed
-		n_passed=`expr $n_passed + 1`
 	fi
 fi
 testNum=`expr $testNum + 1`
 rm -f tests.result
-echo $n_failed Tests Failed >tests.result
-echo $n_passed Tests Passed >>tests.result
-echo $n_unchecked Tests Unchecked >>tests.result
+echo $n_failed Tests Failed 		>  tests.result
+echo $n_passed Tests Passed 		>> tests.result
+echo $n_unchecked Tests Unchecked	>> tests.result
 done
 ./testCleanup.sh > /dev/null
 cat tests.result
--- test/storage/testSetup.sh
+++ test/storage/testSetup.sh
@@ -1,4 +1,6 @@
 psql << EOF
+drop table if exists foglamp.test;
+
 create table foglamp.test (
 	id	bigint,
 	key	character(5),
@@ -7,4 +9,6 @@ create table foglamp.test (
 );
 
 insert into foglamp.test values (1, 'TEST1',  'A test row', '{ "json" : "test1" }');
+
+delete from readings;
 EOF

9f2919ca1fda1775a01b1694216cb12c16d78bad
ashwinscale <ashwin@dianomic.com>
2017-10-30 11:41:47 -0700

    FOGL-650 cleanup

--- src/sql/foglamp_init_data.sql
+++ src/sql/foglamp_init_data.sql
@@ -78,7 +78,6 @@ INSERT INTO foglamp.configuration ( key, description, value )
 --        last purge: ts of the last purge call
 INSERT INTO foglamp.configuration ( key, description, value )
      VALUES ( 'COAP', 'CoAP Plugin Configuration', ' { "plugin" : { "type" : "string", "value" : "foglamp.device.coap_device", "default" : "foglamp.device.coap_device", "description" : "Python module name of the plugin to load" } } ');
---     VALUES ( 'COAP', 'CoAP Plugin Configuration', to_jsonb( '{ "plugin" : { "type" : "string", "value" : "foglamp.device.coap_device", "default" : "foglamp.device.coap_device", "description" : "Python module name of the plugin to load" } }'::TEXT ) );
 
 -- DELETE data for roles, resources and permissions
 DELETE FROM foglamp.role_resource_permission;

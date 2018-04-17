5e19f1b3f8c0ef49e5ede0116cfa39cdb969b150
Stefano Simonelli <stefano@dianomic.com>
2018-04-17 10:26:18 +0200

    FOGL-1264: fix the empty north plugin

--- python/foglamp/plugins/north/empty/empty.py
+++ python/foglamp/plugins/north/empty/empty.py
@@ -5,7 +5,7 @@
 
 # Module information
 __author__ = "Stefano Simonelli"
-__copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
+__copyright__ = "Copyright (c) 2017,2018 OSIsoft, LLC"
 __license__ = "Apache 2.0"
 __version__ = "${VERSION}"
 
@@ -17,40 +17,50 @@ import foglamp.plugins.north.common.exceptions as plugin_exceptions
 from foglamp.common import logger
 
 
-def plugin_retrieve_info(stream_id):
+def plugin_info():
     """ Empty North Plugin
 
     Returns:
         plugin_info
     """
 
-    plugin_info = {
+    _plugin_info = {
         'name': _MODULE_NAME,
         'version': "1.0.0",
         'type': "north",
         'interface': "1.0",
-        'config': stream_id
+        'config': "none"
     }
 
-    return plugin_info
+    return _plugin_info
 
 
-def plugin_init():
+def plugin_init(data):
     """ Empty North Plugin """
+
+    _config = {}
+
+    return _config
 
 
 def plugin_send(raw_data, stream_id):
     """ Empty North Plugin """
 
-    data_sent = ()
+    is_data_sent = True
     new_position = 0
     num_sent = 0
 
-    return data_sent, new_position, num_sent
+    return is_data_sent, new_position, num_sent
 
 
 def plugin_shutdown():
     """ Empty North Plugin """
+    pass
+
+
+def plugin_reconfigure():
+    """ Empty North Plugin """
+    pass
 
 
 if __name__ == "__main__":

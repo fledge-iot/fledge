60e6f3c851cb4282fb8466095a36d639597767c5
ashish-jabble <ashish@dianomic.com>
2018-05-29 12:28:58 +0530

    logger fixes for test_instance

--- tests/unit/python/foglamp/services/common/microservice_management/test_instance.py
+++ tests/unit/python/foglamp/services/common/microservice_management/test_instance.py
@@ -5,7 +5,9 @@
 # FOGLAMP_END
 
 import uuid
+from unittest.mock import patch
 import pytest
+
 from foglamp.services.core.service_registry.service_registry import ServiceRegistry as Service
 from foglamp.services.core.service_registry.exceptions import *
 from foglamp.common.service_record import ServiceRecord
@@ -18,7 +20,7 @@ __version__ = "${VERSION}"
 
 
 @pytest.allure.feature("unit")
-@pytest.allure.story("service-registry instance")
+@pytest.allure.story("services", "common", "microservice_management")
 class TestInstance:
     def setup_method(self):
         Service._registry = []
@@ -27,26 +29,37 @@ class TestInstance:
         Service._registry = []
 
     async def test_register(self):
-        idx = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
-        assert str(uuid.UUID(idx, version=4)) == idx
+        with patch.object(Service._logger, 'info') as log_info:
+            idx = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
+            assert str(uuid.UUID(idx, version=4)) == idx
+        assert 1 == log_info.call_count
 
     async def test_duplicate_name_registration(self):
-        idx1 = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
-        assert str(uuid.UUID(idx1, version=4)) == idx1
+        with patch.object(Service._logger, 'info') as log_info:
+            idx1 = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
+            assert str(uuid.UUID(idx1, version=4)) == idx1
+        assert 1 == log_info.call_count
+
         with pytest.raises(AlreadyExistsWithTheSameName) as excinfo:
             Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
         assert str(excinfo).endswith('AlreadyExistsWithTheSameName')
 
     async def test_duplicate_address_port_registration(self):
-        idx1 = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
-        assert str(uuid.UUID(idx1, version=4)) == idx1
+        with patch.object(Service._logger, 'info') as log_info:
+            idx1 = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
+            assert str(uuid.UUID(idx1, version=4)) == idx1
+        assert 1 == log_info.call_count
+
         with pytest.raises(AlreadyExistsWithTheSameAddressAndPort) as excinfo:
             Service.register("StorageService2", "Storage", "127.0.0.1", 9999, 1998)
         assert str(excinfo).endswith('AlreadyExistsWithTheSameAddressAndPort')
 
     async def test_duplicate_address_and_mgt_port_registration(self):
-        idx1 = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
-        assert str(uuid.UUID(idx1, version=4)) == idx1
+        with patch.object(Service._logger, 'info') as log_info:
+            idx1 = Service.register("StorageService1", "Storage", "127.0.0.1", 9999, 1999)
+            assert str(uuid.UUID(idx1, version=4)) == idx1
+        assert 1 == log_info.call_count
+
         with pytest.raises(AlreadyExistsWithTheSameAddressAndManagementPort) as excinfo:
             Service.register("StorageService2", "Storage", "127.0.0.1", 9998, 1999)
         assert str(excinfo).endswith('AlreadyExistsWithTheSameAddressAndManagementPort')
@@ -68,55 +81,63 @@ class TestInstance:
 
     async def test_unregister(self, mocker):
         # register a service
-        idx = Service.register("StorageService2", "Storage", "127.0.0.1", 8888, 1888)
-        assert str(uuid.UUID(idx, version=4)) == idx
+        with patch.object(Service._logger, 'info') as log_info:
+            idx = Service.register("StorageService2", "Storage", "127.0.0.1", 8888, 1888)
+            assert str(uuid.UUID(idx, version=4)) == idx
+        assert 1 == log_info.call_count
 
         mocker.patch.object(InterestRegistry, '__init__', return_value=None)
         mocker.patch.object(InterestRegistry, 'get', return_value=list())
 
         # deregister the same
-        t = Service.unregister(idx)
-        assert idx == t
+        with patch.object(Service._logger, 'info') as log_info2:
+            t = Service.unregister(idx)
+            assert idx == t
+        assert 1 == log_info2.call_count
 
         s = Service.get(idx)
         assert s[0]._status == 2  # Unregistered
 
     async def test_get(self):
-        s = Service.register("StorageService", "Storage", "localhost", 8881, 1888)
-        c = Service.register("CoreService", "Core", "localhost", 7771, 1777)
-        d = Service.register("SouthService", "Southbound", "127.0.0.1", 9991, 1999, "https")
+        with patch.object(Service._logger, 'info') as log_info:
+            s = Service.register("StorageService", "Storage", "localhost", 8881, 1888)
+            c = Service.register("CoreService", "Core", "localhost", 7771, 1777)
+            d = Service.register("SouthService", "Southbound", "127.0.0.1", 9991, 1999, "https")
+        assert 3 == log_info.call_count
 
-        l = Service.get()
-        assert 3 == len(l)
+        _service = Service.get()
+        assert 3 == len(_service)
 
-        assert s == l[0]._id
-        assert "StorageService" == l[0]._name
-        assert "Storage" == l[0]._type
-        assert "localhost" == l[0]._address
-        assert 8881 == int(l[0]._port)
-        assert 1888 == int(l[0]._management_port)
+        assert s == _service[0]._id
+        assert "StorageService" == _service[0]._name
+        assert "Storage" == _service[0]._type
+        assert "localhost" == _service[0]._address
+        assert 8881 == int(_service[0]._port)
+        assert 1888 == int(_service[0]._management_port)
         # validates default set to HTTP
-        assert "http" == l[0]._protocol
+        assert "http" == _service[0]._protocol
 
-        assert c == l[1]._id
-        assert "CoreService" == l[1]._name
-        assert "Core" == l[1]._type
-        assert "localhost" == l[1]._address
-        assert 7771 == int(l[1]._port)
-        assert 1777 == int(l[1]._management_port)
+        assert c == _service[1]._id
+        assert "CoreService" == _service[1]._name
+        assert "Core" == _service[1]._type
+        assert "localhost" == _service[1]._address
+        assert 7771 == int(_service[1]._port)
+        assert 1777 == int(_service[1]._management_port)
         # validates default set to HTTP
-        assert "http" == l[1]._protocol
+        assert "http" == _service[1]._protocol
 
-        assert d == l[2]._id
-        assert "SouthService" == l[2]._name
-        assert "Southbound" == l[2]._type
-        assert "127.0.0.1" == l[2]._address
-        assert 9991 == int(l[2]._port)
-        assert 1999 == int(l[2]._management_port)
-        assert "https" == l[2]._protocol
+        assert d == _service[2]._id
+        assert "SouthService" == _service[2]._name
+        assert "Southbound" == _service[2]._type
+        assert "127.0.0.1" == _service[2]._address
+        assert 9991 == int(_service[2]._port)
+        assert 1999 == int(_service[2]._management_port)
+        assert "https" == _service[2]._protocol
 
     async def test_get_fail(self):
         with pytest.raises(DoesNotExist) as excinfo:
-            Service.register("StorageService", "Storage", "127.0.0.1", 8888, 9999)
-            Service.get('incorrect_id')
+            with patch.object(Service._logger, 'info') as log_info:
+                Service.register("StorageService", "Storage", "127.0.0.1", 8888, 9999)
+                Service.get('incorrect_id')
+            assert 1 == log_info.call_count
         assert str(excinfo).endswith('DoesNotExist')

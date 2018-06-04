70ee3738834e8b7d457d0a7c025bd35cc77296ac
Vaibhav Singhal <vaibhav@scaledb.com>
2018-06-04 16:10:29 +0530

    Test fixed for async stats functionality

--- tests/unit/python/foglamp/common/test_statistics.py
+++ tests/unit/python/foglamp/common/test_statistics.py
@@ -4,14 +4,13 @@
 # See: http://foglamp.readthedocs.io/
 # FOGLAMP_END
 
-import asyncio
 import json
 
 from unittest.mock import MagicMock, patch
 import pytest
 
-from foglamp.common.statistics import Statistics, _logger
-from foglamp.common.storage_client.storage_client import StorageClient
+from foglamp.common import statistics
+from foglamp.common.storage_client.storage_client import StorageClientAsync as StorageClient
 
 
 __author__ = "Ashish Jabble, Mark Riddoch, Vaibhav Singhal"
@@ -24,56 +23,64 @@ __version__ = "${VERSION}"
 @pytest.allure.story("common", "statistics")
 class TestStatistics:
 
-    def test_init_with_no_storage(self):
+    async def test_init_with_no_storage(self):
         storage_client_mock = None
         with pytest.raises(TypeError) as excinfo:
-            Statistics(storage_client_mock)
-        assert str(excinfo.value) == 'Must be a valid Storage object'
+            await statistics.create_statistics(storage_client_mock)
+        assert str(excinfo.value) == 'Must be a valid Async Storage object'
 
-    def test_init_with_storage(self):
+    async def test_init_with_storage(self):
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
-        assert isinstance(s, Statistics)
+        s = await statistics.create_statistics(storage_client_mock)
+        assert isinstance(s, statistics.Statistics)
         assert isinstance(s._storage, StorageClient)
 
-    def test_singleton(self):
+    async def test_singleton(self):
         """ Test that two statistics instance share the same state """
         storageMock1 = MagicMock(spec=StorageClient)
-        s1 = Statistics(storageMock1)
+        s1 = await statistics.create_statistics(storageMock1)
         storageMock2 = MagicMock(spec=StorageClient)
-        s2 = Statistics(storageMock2)
+        s2 = await statistics.create_statistics(storageMock2)
         assert s1._storage == s2._storage
 
-    def test_register(self):
+    async def test_register(self):
         """ Test that register results in a database insert """
         storageMock = MagicMock(spec=StorageClient)
-        stats = Statistics(storageMock)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(stats.register('T1Stat', 'Test stat'))
-        args, kwargs = stats._storage.insert_into_tbl.call_args
-        assert args[0] == 'statistics'
-        expected_storage_args = json.loads(args[1])
-        assert expected_storage_args['key'] == 'T1Stat'
-        assert expected_storage_args['value'] == 0
-        assert expected_storage_args['previous_value'] == 0
-        assert expected_storage_args['description'] == 'Test stat'
+        stats = statistics.Statistics(storageMock)
+        stats._registered_keys = []
+        async def mock_coro():
+            return {"response": "updated", "rows_affected": 1}
+
+        with patch.object(stats._storage, 'insert_into_tbl', return_value=mock_coro()) as stat_update:
+            await stats.register('T1Stat', 'Test stat')
+            args, kwargs = stat_update.call_args
+            assert args[0] == 'statistics'
+            expected_storage_args = json.loads(args[1])
+            assert expected_storage_args['key'] == 'T1Stat'
+            assert expected_storage_args['value'] == 0
+            assert expected_storage_args['previous_value'] == 0
+            assert expected_storage_args['description'] == 'Test stat'
         stats._storage.insert_into_tbl.reset_mock()
 
-    def test_register_twice(self):
+    async def test_register_twice(self):
         """ Test that register results in a database insert only once for same key"""
         storageMock = MagicMock(spec=StorageClient)
-        stats = Statistics(storageMock)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(stats.register('T2Stat', 'Test stat'))
-        loop.run_until_complete(stats.register('T2Stat', 'Test stat'))
-        assert stats._storage.insert_into_tbl.called
-        assert stats._storage.insert_into_tbl.call_count == 1
+        stats = statistics.Statistics(storageMock)
+        stats._registered_keys = []
+        async def mock_coro():
+            return {"response": "updated", "rows_affected": 1}
+
+        with patch.object(stats._storage, 'insert_into_tbl', return_value=mock_coro()) as stat_update:
+            await stats.register('T2Stat', 'Test stat')
+            await stats.register('T2Stat', 'Test stat')
+            assert stat_update.called
+            assert stat_update.call_count == 1
         stats._storage.insert_into_tbl.reset_mock()
 
     async def test_register_exception(self):
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
-        with patch.object(_logger, 'exception') as logger_exception:
+        s = statistics.Statistics(storage_client_mock)
+        with patch.object(statistics._logger, 'exception') as logger_exception:
             with patch.object(s._storage, 'insert_into_tbl', side_effect=Exception):
                 with pytest.raises(Exception):
                     await s.register('T3Stat', 'Test stat')
@@ -81,35 +88,43 @@ class TestStatistics:
         assert args[0] == 'Unable to create new statistic %s, error %s'
         assert args[1] == 'T3Stat'
 
-    def test_load_keys(self):
+    async def test_load_keys(self):
         """Test the load key"""
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
-        storage_return = {'rows': [{"previous_value": 0, "value": 1,
-                                    "key": "K1", "description": "desc1"}]}
-        with patch.object(s._storage, 'query_tbl_with_payload', return_value=storage_return) as patch_query_tbl:
-            s._load_keys()
+        s = statistics.Statistics(storage_client_mock)
+        s._registered_keys = []
+
+        async def mock_coro():
+            return {'rows': [{"previous_value": 0, "value": 1,
+                              "key": "K1", "description": "desc1"}]}
+        with patch.object(s._storage, 'query_tbl_with_payload', return_value=mock_coro()) as patch_query_tbl:
+            await s._load_keys()
             assert "K1" in s._registered_keys
         patch_query_tbl.assert_called_once_with('statistics', '{"return": ["key"]}')
 
     async def test_load_keys_exception(self):
         """Test the load key exception"""
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
-        with patch.object(_logger, 'exception') as logger_exception:
-            with patch.object(s._storage, 'query_tbl_with_payload', side_effect=Exception):
-                with pytest.raises(Exception):
-                    await s._load_keys()
+        s = statistics.Statistics(storage_client_mock)
+        s._registered_keys = []
+
+        async def mock_coro():
+            return Exception
+        with patch.object(statistics._logger, 'exception') as logger_exception:
+            with patch.object(s._storage, 'query_tbl_with_payload', return_value=mock_coro()):
+                await s._load_keys()
         args, kwargs = logger_exception.call_args
         assert args[0] == 'Failed to retrieve statistics keys, %s'
 
     async def test_update(self):
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
+        s = statistics.Statistics(storage_client_mock)
+        async def mock_coro():
+            return {"response": "updated", "rows_affected": 1}
         payload = '{"where": {"column": "key", "condition": "=", "value": "READING"}, ' \
                   '"expressions": [{"column": "value", "operator": "+", "value": 5}]}'
         expected_result = {"response": "updated", "rows_affected": 1}
-        with patch.object(s._storage, 'update_tbl', return_value=expected_result) as stat_update:
+        with patch.object(s._storage, 'update_tbl', return_value=mock_coro()) as stat_update:
             await s.update('READING', 5)
             assert expected_result['response'] == "updated"
         stat_update.assert_called_once_with('statistics', payload)
@@ -123,7 +138,7 @@ class TestStatistics:
     ])
     async def test_update_with_invalid_params(self, key, value_increment, exception_name, exception_message):
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
+        s = statistics.Statistics(storage_client_mock)
 
         with pytest.raises(exception_name) as excinfo:
             await s.update(key, value_increment)
@@ -131,34 +146,39 @@ class TestStatistics:
 
     async def test_update_exception(self):
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
+        s = statistics.Statistics(storage_client_mock)
         msg = 'Unable to update statistics value based on statistics_key %s and value_increment %d,' \
               ' error %s', 'BUFFERED', 5, ''
         with patch.object(s._storage, 'update_tbl', side_effect=Exception()):
             with pytest.raises(Exception):
-                with patch.object(_logger, 'exception') as logger_exception:
+                with patch.object(statistics._logger, 'exception') as logger_exception:
                     await s.update('BUFFERED', 5)
             logger_exception.assert_called_once_with(*msg)
 
     async def test_add_update(self):
         stat_dict = {'FOGBENCH/TEMPERATURE': 1}
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
+        s = statistics.Statistics(storage_client_mock)
         payload = '{"where": {"column": "key", "condition": "=", "value": "FOGBENCH/TEMPERATURE"}, ' \
                   '"expressions": [{"column": "value", "operator": "+", "value": 1}]}'
-        expected_result = {"response": "updated", "rows_affected": 1}
-        with patch.object(s._storage, 'update_tbl', return_value=expected_result) as stat_update:
+        async def mock_coro():
+            return {"response": "updated", "rows_affected": 1}
+
+        with patch.object(s._storage, 'update_tbl', return_value=mock_coro()) as stat_update:
             await s.add_update(stat_dict)
-            assert expected_result['response'] == "updated"
         stat_update.assert_called_once_with('statistics', payload)
 
     async def test_insert_when_key_error(self):
         stat_dict = {'FOGBENCH/TEMPERATURE': 1}
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
-        with patch.object(_logger, 'exception') as logger_exception:
-            with pytest.raises(KeyError):
-                await s.add_update(stat_dict)
+        s = statistics.Statistics(storage_client_mock)
+        async def mock_coro():
+            return {"response": "not updated", "rows_affected": 0}
+
+        with patch.object(s._storage, 'update_tbl', return_value=mock_coro()) as stat_update:
+            with patch.object(statistics._logger, 'exception') as logger_exception:
+                with pytest.raises(KeyError):
+                    await s.add_update(stat_dict)
         args, kwargs = logger_exception.call_args
         assert args[0] == 'Statistics key %s has not been registered'
         assert args[1] == 'FOGBENCH/TEMPERATURE'
@@ -166,11 +186,11 @@ class TestStatistics:
     async def test_add_update_exception(self):
         stat_dict = {'FOGBENCH/TEMPERATURE': 1}
         storage_client_mock = MagicMock(spec=StorageClient)
-        s = Statistics(storage_client_mock)
+        s = statistics.Statistics(storage_client_mock)
         msg = 'Unable to update statistics value based on statistics_key %s and value_increment' \
               ' %s, error %s', "FOGBENCH/TEMPERATURE", 1, ''
         with patch.object(s._storage, 'update_tbl', side_effect=Exception()):
             with pytest.raises(Exception):
-                with patch.object(_logger, 'exception') as logger_exception:
+                with patch.object(statistics._logger, 'exception') as logger_exception:
                     await s.add_update(stat_dict)
                 logger_exception.assert_called_once_with(*msg)

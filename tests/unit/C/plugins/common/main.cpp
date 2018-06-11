f07a1e9350db3996093ebd215d7c62f3528c645e
Massimiliano Pinto <massimiliano.pinto@gmail.com>
2018-06-11 14:47:41 +0200

    FOGL-1330: fist set of OMF transaltion tests
    FOGL-1330: fist set of OMF transaltion tests

---
+++ tests/unit/C/plugins/common/main.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <resultset.h>
+#include <string.h>
+#include <string>
+
+/*
+ * FogLAMP Readings to OMF translation unit tests
+ *
+ * Copyright (c) 2018 Dianomic Systems
+ *
+ * Released under the Apache 2.0 Licence
+ *
+ * Author: Massimiliano Pinto
+ */
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+

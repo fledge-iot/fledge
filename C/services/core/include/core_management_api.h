f2fc2ec871c55b7671ae1dad28a6307210839f7b
Mark Riddoch <mark@dianomic.com>
2018-06-27 12:42:28 +0100

    FOGL-1464

---
+++ C/services/core/include/core_management_api.h
@@ -0,0 +1,43 @@
+#ifndef _CORE_MANAGEMENT_API_H
+#define _CORE_MANAGEMENT_API_H
+/*
+ * FogLAMP core microservice management API.
+ *
+ * Copyright (c) 2018 Dianomic Systems
+ *
+ * Released under the Apache 2.0 Licence
+ *
+ * Author: Mark Riddoch
+ */
+#include <management_api.h>
+
+#define REGISTER_SERVICE	"/foglamp/service/register"
+
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+/**
+ * Management API server for a C++ microservice
+ */
+class CoreManagementApi : public ManagementApi {
+	public:
+		CoreManagementApi(const std::string& name, const unsigned short port);
+		~CoreManagementApi();
+		static CoreManagementApi *getInstance();
+		void			registerMicroService(std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request);
+	private:
+		static CoreManagementApi *m_instance;
+		void 		errorResponse(
+					std::shared_ptr<HttpServer::Response> response,
+		               		SimpleWeb::StatusCode statusCode,
+					const std::string& entryPoint,
+					const std::string& msg);
+		void			internalError(std::shared_ptr<HttpServer::Response>, const std::exception&);
+		void		respond(
+					std::shared_ptr<HttpServer::Response> response,
+					SimpleWeb::StatusCode statusCode,
+					const std::string& payload);
+		void		respond(
+					std::shared_ptr<HttpServer::Response> response,
+					const std::string& payload);
+};
+#endif

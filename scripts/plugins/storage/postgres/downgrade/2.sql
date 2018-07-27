c624c437cf47b31389afc4bc4687d654ba2a11a2
Amandeep Singh Arora <aman@dianomic.com>
2018-07-27 08:40:01 +0000

    Changing case of SQL commands

--- scripts/plugins/storage/postgres/downgrade/2.sql
+++ scripts/plugins/storage/postgres/downgrade/2.sql
@@ -3,6 +3,6 @@ UPDATE foglamp.configuration SET key = 'SEND_PR_2' WHERE key = 'North Statistics
 UPDATE foglamp.configuration SET key = 'SEND_PR_4' WHERE key = 'North Readings to OCS';
 
 -- Remove DHT11 C++ south plugin entries
-delete from foglamp.configuration where key = 'dht11';
-delete from foglamp.scheduled_processes where name='dht11';
-delete from foglamp.schedules where process_name = 'dht11';
+DELETE FROM foglamp.configuration WHERE key = 'dht11';
+DELETE FROM foglamp.scheduled_processes WHERE name='dht11';
+DELETE FROM foglamp.schedules WHERE process_name = 'dht11';

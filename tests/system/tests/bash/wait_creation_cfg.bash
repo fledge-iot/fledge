9973fa159ad28e39e9e0f77b18d541c58ff9ba15
Stefano Simonelli <stefano@dianomic.com>
2018-08-03 11:51:04 +0200

    FOGL-1466: Working stage - adapting the tests in relation to the changes of FOGL-1560

--- tests/system/tests/bash/wait_creation_cfg.bash
+++ tests/system/tests/bash/wait_creation_cfg.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # It waits until either the requested FogLAMP configuration is created or it reaches the timeout.
+count=1
 while [ true ]
 do
     curl -s -X GET http://${FOGLAMP_SERVER}:${FOGLAMP_PORT}/foglamp/category/${1}  | jq '.value'  > /dev/null 2>&1
@@ -8,13 +9,16 @@ do
 
     if [[ "$result" == "0" ]]
     then
+        echo "FogLAMP configuration :${1}: available  - N. of retries :${count}:"
         exit 0
     else
         if [[ $count -le ${RETRY_COUNT} ]]
         then
+            echo "FogLAMP configuration :${1}: not available, result :${result}: - N. of retries :${count}:"
             sleep 1
             count=$((count+1))
         else
+            echo "FogLAMP plugin :${1}: not available  - N. of retries :${count}:"
             exit 1
         fi
     fi

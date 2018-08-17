70d7159e01bc86483fcf43f313bef3478ab16ab7
ashish-jabble <ashish@dianomic.com>
2018-08-17 15:47:16 +0530

    fixed function call hoping for ingest and id and index added for asset_tracker schema

--- scripts/plugins/storage/sqlite/init.sql
+++ scripts/plugins/storage/sqlite/init.sql
@@ -567,12 +567,16 @@ CREATE TABLE foglamp.category_children (
 
 -- Create the asset_tracker table
 CREATE TABLE foglamp.asset_tracker (
+       id            integer          PRIMARY KEY AUTOINCREMENT,
        asset         character(50)    NOT NULL,
        event         character varying(50) NOT NULL,
        service       character varying(255) NOT NULL,
        foglamp       character varying(50) NOT NULL,
        plugin        character varying(50) NOT NULL,
        ts            DATETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'localtime')) );
+
+CREATE INDEX asset_tracker_ix1 ON asset_tracker (asset);
+CREATE INDEX asset_tracker_ix2 ON asset_tracker (service);
 
 ----------------------------------------------------------------------
 -- Initialization phase - DML

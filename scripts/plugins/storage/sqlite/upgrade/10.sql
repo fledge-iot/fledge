8d9623285588c4de9460c74455a3d2adadafc620
Praveen Garg <praveen.garg@nerdapplabs.com>
2018-08-24 15:34:09 +0530

    removed stats keys hard coded for plugins

---
+++ scripts/plugins/storage/sqlite/upgrade/10.sql
@@ -0,0 +1,8 @@
+DELETE FROM foglamp.statistics WHERE key IN (
+    'NORTH_READINGS_TO_PI',
+    'NORTH_STATISTICS_TO_PI',
+    'NORTH_READINGS_TO_HTTP',
+    'North Readings to PI',
+    'North Statistics to PI',
+    'North Readings to OCS'
+    );

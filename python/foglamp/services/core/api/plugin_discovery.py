aeffcd5581c089e1c1d5db443866b8e9faf79825
ashish-jabble <ashish@dianomic.com>
2018-09-25 14:38:18 +0530

    default configuration added to plugins installed when config param is true

--- python/foglamp/services/core/api/plugin_discovery.py
+++ python/foglamp/services/core/api/plugin_discovery.py
@@ -25,16 +25,27 @@ async def get_plugins_installed(request):
 
     :Example:
         curl -X GET http://localhost:8081/foglamp/plugins/installed
+        curl -X GET http://localhost:8081/foglamp/plugins/installed?config=true
         curl -X GET http://localhost:8081/foglamp/plugins/installed?type=north
+        curl -X 'GET http://localhost:8081/foglamp/plugins/installed?type=north&config=true'
     """
 
     plugin_type = None
+    is_config = False
     if 'type' in request.query and request.query['type'] != '':
         plugin_type = request.query['type'].lower()
 
     if plugin_type is not None and plugin_type not in ['north', 'south']:
         raise web.HTTPBadRequest(reason="Invalid plugin type. Must be 'north' or 'south'.")
 
-    plugins_list = PluginDiscovery.get_plugins_installed(plugin_type)
+    if 'config' in request.query:
+        config = request.query['config']
+        if config not in ['true', 'false', True, False]:
+                raise web.HTTPBadRequest(reason='Only "true", "false", true, false'
+                                                ' are allowed for value of config.')
+        is_config = True if ((type(config) is str and config.lower() in ['true']) or (
+            (type(config) is bool and config is True))) else False
+
+    plugins_list = PluginDiscovery.get_plugins_installed(plugin_type, is_config)
 
     return web.json_response({"plugins": plugins_list})

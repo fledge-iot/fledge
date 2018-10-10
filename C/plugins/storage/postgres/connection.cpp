039a78755408b81964498f29a46f24a2a63694f7
Stefano <stefano@dianomic.com>
2018-10-10 14:31:01 +0100

    FOGL-1861 : postgress fixed

--- C/plugins/storage/postgres/connection.cpp
+++ C/plugins/storage/postgres/connection.cpp
@@ -160,8 +160,7 @@ SQLBuffer	jsonConstraints;	// Extra constraints to add to where clause
 								sql.append((*itr)["column"].GetString());
 								sql.append(" AT TIME ZONE '");
 								sql.append((*itr)["timezone"].GetString());
-								sql.append("' AS ");
-								sql.append((*itr)["column"].GetString());
+								sql.append("' ");
 							}
 							else
 							{

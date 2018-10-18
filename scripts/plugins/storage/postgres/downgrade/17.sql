80d17b78b9e55bd48ef09d9065abee9db7be6e74
amarendra-dianomic <amarendra@dianomic.com>
2018-10-18 14:41:16 +0530

    Extra comments removed from upgrade/downgrade scripts

--- scripts/plugins/storage/postgres/downgrade/17.sql
+++ scripts/plugins/storage/postgres/downgrade/17.sql
@@ -1,6 +1,3 @@
---ALTER TABLE foglamp.tasks DROP CONSTRAINT schedules_name_uniquq;
---ALTER TABLE foglamp.tasks DROP CONSTRAINT tasks_fk2;
 ALTER TABLE foglamp.tasks DROP COLUMN schedule_name;
---DROP INDEX IF EXISTS foglamp.tasks_ix1;
 CREATE INDEX tasks_ix1
    ON foglamp.tasks(process_name, start_time);

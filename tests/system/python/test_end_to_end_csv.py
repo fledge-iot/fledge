ab88eb95e75529af49bfff36d071aa3e459a487f
Vaibhav Singhal <vaibhav@scaledb.com>
2018-12-28 12:40:19 +0530

    Feedback changes

--- tests/system/python/test_end_to_end_csv.py
+++ tests/system/python/test_end_to_end_csv.py
@@ -26,19 +26,18 @@ __version__ = "${VERSION}"
 CSV_NAME = "sample.csv"
 CSV_HEADER = "temperature"
 CSV_DATA = 2.5
-SENSOR_NAME = CSV_HEADER
 
 # Number of tries to make to read PI data and to read North configuration
 NUM_RETRIES = 3
-# Sleep interval between each tries
-SLEEP_INTERVAL = 10
+# Low sleep interval between each tries, used in internal foglamp process waits
+SLEEP_INTERVAL_LOW = 3
+# High sleep interval between each tries, used in external pi webapi waits
+SLEEP_INTERVAL_HIGH = 10
 
 
-@pytest.fixture
-def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp_url, pi_host, pi_port,
-                      north_plugin, pi_token):
-    """ This fixture clone a south repo and starts both south and north instance """
-    assert os.environ.get('FOGLAMP_ROOT') is not None
+def _start_folamp_south(south_plugin, asset_name, foglamp_url):
+    """Start south service"""
+
     south_config = {"assetName": {"value": "{}".format(asset_name)}, "csvFilename": {"value": "{}".format(CSV_NAME)},
                     "ingestMode": {"value": "batch"}}
     data = {"name": "play", "type": "South", "plugin": "{}".format(south_plugin), "enabled": "true",
@@ -49,7 +48,8 @@ def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp
     subprocess.run(["rm -rf -d ${FOGLAMP_ROOT}/python/foglamp/plugins/south/*/"], shell=True, check=True)
     subprocess.run(["git clone https://github.com/foglamp/foglamp-south-{}.git /tmp/foglamp-south-{}".
                    format(south_plugin, south_plugin)], shell=True, check=True)
-    subprocess.run(["cp -r /tmp/foglamp-south-{}/python/foglamp/plugins/south/* $FOGLAMP_ROOT/python/foglamp/plugins/south/".format(south_plugin)], shell=True, check=True)
+    subprocess.run(["cp -r /tmp/foglamp-south-{}/python/foglamp/plugins/south/* "
+                    "$FOGLAMP_ROOT/python/foglamp/plugins/south/".format(south_plugin)], shell=True, check=True)
     subprocess.run(["rm -rf $FOGLAMP_ROOT/data/{}".format(CSV_NAME)], shell=True, check=True)
     csv_file_path = os.path.join(os.path.expandvars('${FOGLAMP_ROOT}'), 'data/{}'.format(CSV_NAME))
     f = open(csv_file_path, "w")
@@ -63,7 +63,13 @@ def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp
     r = r.read().decode()
     retval = json.loads(r)
     assert retval["name"] == "play"
+    return csv_file_path
 
+
+def _start_folamp_north(foglamp_url, pi_host, pi_port, north_plugin, pi_token):
+    """Start north service"""
+
+    conn = http.client.HTTPConnection(foglamp_url)
     data = {"name": "North_Readings_to_PI",
             "plugin": "{}".format(north_plugin),
             "type": "north",
@@ -79,6 +85,7 @@ def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp
     retval = json.loads(r)
     assert retval["name"] == "North_Readings_to_PI"
 
+    # Test the north is setup and configuration is available
     retry_count = 0
     config_status = None
     while config_status != 200 and retry_count < NUM_RETRIES:
@@ -87,7 +94,7 @@ def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp
         config_status = r.status
         r.read().decode()
         retry_count += 1
-        time.sleep(SLEEP_INTERVAL)
+        time.sleep(SLEEP_INTERVAL_LOW)
 
     if config_status != 200 or retry_count == NUM_RETRIES:
         assert False, "North plugin config load failed in {} tries".format(NUM_RETRIES)
@@ -103,12 +110,9 @@ def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp
     r = conn.getresponse()
     assert 200 == r.status
     r.read().decode()
-    yield start_south_north
-    _remove_csv_files(csv_file_path)
-    _remove_directories("/tmp/foglamp-south-{}".format(south_plugin))
 
 
-def _remove_csv_files(file_path=None):
+def _remove_data_file(file_path=None):
     if os.path.exists(file_path):
         os.remove(file_path)
 
@@ -118,12 +122,18 @@ def _remove_directories(dir_path=None):
         shutil.rmtree(dir_path, ignore_errors=True)
 
 
-def _read_data_from_pi(host, port, admin, password, pi_database, asset):
+def _read_data_from_pi(host, admin, password, pi_database, asset):
     """ This method reads data from pi web api """
+
+    # List of pi databases
     dbs = None
+    # PI logical grouping of attributes and child elements
     element = None
+    # List of elements
     url_elements_list = None
+    # Element's EndValue
     url_assets_list = None
+    # Resources in the PI Web API are addressed by WebID, parameter used for deletion of element
     web_id = None
 
     username_password = "{}:{}".format(admin, password)
@@ -167,7 +177,7 @@ def _read_data_from_pi(host, port, admin, password, pi_database, asset):
             r = json.loads(res.read().decode())
             _items = r["Items"]
             for el in _items:
-                if el["Name"] == SENSOR_NAME:
+                if el["Name"] == CSV_HEADER:
                     value = el["Value"]["Value"]
                     # After Value is stored, delete this element
                     conn.request("DELETE", '/piwebapi/elements/{}'.format(web_id), headers=headers)
@@ -178,10 +188,33 @@ def _read_data_from_pi(host, port, admin, password, pi_database, asset):
         return None
 
 
+@pytest.fixture
+def start_south_north(reset_and_start_foglamp, south_plugin, asset_name, foglamp_url, pi_host, pi_port,
+                      north_plugin, pi_token):
+    """ This fixture clone a south repo and starts both south and north instance """
+
+    # Test that FOGLAMP_ROOT environment variable is set
+    assert os.environ.get('FOGLAMP_ROOT') is not None
+
+    # Start foglamp south service
+    csv_file_path = _start_folamp_south(south_plugin, asset_name, foglamp_url)
+
+    # Start foglamp north service
+    _start_folamp_north(foglamp_url, pi_host, pi_port, north_plugin, pi_token)
+
+    # Provide the fixture value
+    yield start_south_north
+
+    # Cleanup code that runs after the caller test is over
+    _remove_data_file(csv_file_path)
+    _remove_directories("/tmp/foglamp-south-{}".format(south_plugin))
+
+
 def test_end_to_end(start_south_north, foglamp_url, pi_host, pi_port, pi_admin, pi_passwd, pi_db, asset_name):
     """ Test that data is inserted in FogLAMP and sent to PI"""
+
     conn = http.client.HTTPConnection(foglamp_url)
-    time.sleep(SLEEP_INTERVAL)
+    time.sleep(SLEEP_INTERVAL_LOW)
     conn.request("GET", '/foglamp/asset')
     r = conn.getresponse()
     assert 200 == r.status
@@ -200,10 +233,11 @@ def test_end_to_end(start_south_north, foglamp_url, pi_host, pi_port, pi_admin, 
     retry_count = 0
     data_from_pi = None
     while data_from_pi is None and retry_count < NUM_RETRIES:
-        data_from_pi = _read_data_from_pi(pi_host, pi_port, pi_admin, pi_passwd, pi_db, asset_name)
+        data_from_pi = _read_data_from_pi(pi_host, pi_admin, pi_passwd, pi_db, asset_name)
         retry_count += 1
-        time.sleep(SLEEP_INTERVAL)
+        time.sleep(SLEEP_INTERVAL_HIGH)
 
     if data_from_pi is None or retry_count == NUM_RETRIES:
         assert False, "Failed to read data from PI"
+
     assert data_from_pi == str(CSV_DATA)

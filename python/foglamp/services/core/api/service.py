6ceb47ee5c1042a54e9a7571b6c11ec19b376fea
ashish-jabble <ashish@dianomic.com>
2019-01-09 16:15:53 +0530

    Ability to start the notification service via add_service REST API call

--- python/foglamp/services/core/api/service.py
+++ python/foglamp/services/core/api/service.py
@@ -9,6 +9,7 @@ import datetime
 import uuid
 from aiohttp import web
 
+from typing import List, Dict, Tuple
 from foglamp.common import utils
 from foglamp.common import logger
 from foglamp.common.service_record import ServiceRecord
@@ -127,6 +128,7 @@ async def add_service(request):
     :Example:
              curl -X POST http://localhost:8081/foglamp/service -d '{"name": "DHT 11", "plugin": "dht11", "type": "south", "enabled": true}'
              curl -sX POST http://localhost:8081/foglamp/service -d '{"name": "Sine", "plugin": "sinusoid", "type": "south", "enabled": true, "config": {"dataPointsPerSec": {"value": "10"}}}' | jq
+             curl -X POST http://localhost:8081/foglamp/service -d '{"name": "NotificationServer", "type": "notification", "enabled": true}' | jq
     """
 
     try:
@@ -142,20 +144,22 @@ async def add_service(request):
 
         if name is None:
             raise web.HTTPBadRequest(reason='Missing name property in payload.')
-        if plugin is None:
-            raise web.HTTPBadRequest(reason='Missing plugin property in payload.')
-        if service_type is None:
-            raise web.HTTPBadRequest(reason='Missing type property in payload.')
         if utils.check_reserved(name) is False:
             raise web.HTTPBadRequest(reason='Invalid name property in payload.')
-        if utils.check_reserved(plugin) is False:
-            raise web.HTTPBadRequest(reason='Invalid plugin property in payload.')
 
+        if service_type is None:
+            raise web.HTTPBadRequest(reason='Missing type property in payload.')
         service_type = str(service_type).lower()
         if service_type == 'north':
             raise web.HTTPNotAcceptable(reason='north type is not supported for the time being.')
-        if service_type not in ['south']:
-            raise web.HTTPBadRequest(reason='Only south type is supported.')
+        if service_type not in ['south', 'notification']:
+            raise web.HTTPBadRequest(reason='Only south and notification type are supported.')
+
+        if plugin is None and service_type == 'south':
+            raise web.HTTPBadRequest(reason='Missing plugin property for type south in payload.')
+        if plugin and utils.check_reserved(plugin) is False:
+            raise web.HTTPBadRequest(reason='Invalid plugin property in payload.')
+
         if enabled is not None:
             if enabled not in ['true', 'false', True, False]:
                 raise web.HTTPBadRequest(reason='Only "true", "false", true, false'
@@ -164,39 +168,27 @@ async def add_service(request):
             (type(enabled) is bool and enabled is True))) else False
 
         # Check if a valid plugin has been provided
-        try:
-            # "plugin_module_path" is fixed by design. It is MANDATORY to keep the plugin in the exactly similar named
-            # folder, within the plugin_module_path.
-            # if multiple plugin with same name are found, then python plugin import will be tried first
-            plugin_module_path = "foglamp.plugins.south" if service_type == 'south' else "foglamp.plugins.north"
-            import_file_name = "{path}.{dir}.{file}".format(path=plugin_module_path, dir=plugin, file=plugin)
-            _plugin = __import__(import_file_name, fromlist=[''])
-
-            # Fetch configuration from the configuration defined in the plugin
-            plugin_info = _plugin.plugin_info()
-            if plugin_info['type'] != service_type:
-                msg = "Plugin of {} type is not supported".format(plugin_info['type'])
-                _logger.exception(msg)
-                return web.HTTPBadRequest(reason=msg)
-            plugin_config = plugin_info['config']
-            process_name = 'south_c' if plugin_info['mode'] == 'poll' else 'south'
-            script = '["services/south_c"]' if plugin_info['mode'] == 'poll' else '["services/south"]'
-        except ImportError as ex:
-            # Checking for C-type plugins
-            plugin_info = apiutils.get_plugin_info(plugin, dir=service_type)
-            if plugin_info['type'] != service_type:
-                msg = "Plugin of {} type is not supported".format(plugin_info['type'])
-                _logger.exception(msg)
-                return web.HTTPBadRequest(reason=msg)
-            plugin_config = plugin_info['config']
-            if not plugin_config:
-                _logger.exception("Plugin %s import problem from path %s. %s", plugin, plugin_module_path, str(ex))
-                raise web.HTTPNotFound(reason='Plugin "{}" import problem from path "{}".'.format(plugin, plugin_module_path))
-            process_name = 'south_c'
-            script = '["services/south_c"]'
-        except Exception as ex:
-            _logger.exception("Failed to fetch plugin configuration. %s", str(ex))
-            raise web.HTTPInternalServerError(reason='Failed to fetch plugin configuration')
+        plugin_module_path, plugin_config, process_name, script = "", {}, "", ""
+        # "plugin_module_path" is fixed by design. It is MANDATORY to keep the plugin in the exactly similar named
+        # folder, within the plugin_module_path.
+        # if multiple plugin with same name are found, then python plugin import will be tried first
+        if service_type == 'south':
+            try:
+                plugin_module_path, plugin_info = load_python_plugin(plugin, service_type)
+                plugin_config = plugin_info['config']
+                process_name = 'south_c' if plugin_info['mode'] == 'poll' else 'south'
+                script = '["services/south_c"]' if plugin_info['mode'] == 'poll' else '["services/south"]'
+            except ImportError as ex:
+                # Checking for C-type plugins
+                plugin_config = load_c_plugin(plugin_module_path, plugin, service_type)
+                process_name = 'south_c'
+                script = '["services/south_c"]'
+            except Exception as ex:
+                _logger.exception("Failed to fetch plugin configuration. %s", str(ex))
+                raise web.HTTPInternalServerError(reason='Failed to fetch plugin configuration')
+        elif service_type == 'notification':
+            process_name = 'notification_c'
+            script = '["services/notification_c"]'
 
         storage = connect.get_storage_async()
         config_mgr = ConfigurationManager(storage)
@@ -225,29 +217,37 @@ async def add_service(request):
                 _logger.exception("Failed to create scheduled process. %s", str(ex))
                 raise web.HTTPInternalServerError(reason='Failed to create service.')
 
+        # check that notification service is not already registered, right now notification service LIMIT to 1
+        if service_type == 'notification':
+            res = await check_notification_schedule(storage)
+            for ps in res['rows']:
+                if 'notification_c' in ps['process_name']:
+                    raise web.HTTPBadRequest(reason='A Notification service already exists.')
+
         # If successful then create a configuration entry from plugin configuration
-        try:
-            # Create a configuration category from the configuration defined in the plugin
-            category_desc = plugin_config['plugin']['description']
-            await config_mgr.create_category(category_name=name,
-                                             category_description=category_desc,
-                                             category_value=plugin_config,
-                                             keep_original_items=True)
-            # Create the parent category for all South services
-            await config_mgr.create_category("South", {}, "South microservices", True)
-            await config_mgr.create_child_category("South", [name])
+        elif service_type == 'south':
+            try:
+                # Create a configuration category from the configuration defined in the plugin
+                category_desc = plugin_config['plugin']['description']
+                await config_mgr.create_category(category_name=name,
+                                                 category_description=category_desc,
+                                                 category_value=plugin_config,
+                                                 keep_original_items=True)
+                # Create the parent category for all South services
+                await config_mgr.create_category("South", {}, "South microservices", True)
+                await config_mgr.create_child_category("South", [name])
 
-            # If config is in POST data, then update the value for each config item
-            if config is not None:
-                if not isinstance(config, dict):
-                    raise ValueError('Config must be a JSON object')
-                for k, v in config.items():
-                    await config_mgr.set_category_item_value_entry(name, k, v['value'])
+                # If config is in POST data, then update the value for each config item
+                if config is not None:
+                    if not isinstance(config, dict):
+                        raise ValueError('Config must be a JSON object')
+                    for k, v in config.items():
+                        await config_mgr.set_category_item_value_entry(name, k, v['value'])
 
-        except Exception as ex:
-            await config_mgr.delete_category_and_children_recursively(name)
-            _logger.exception("Failed to create plugin configuration. %s", str(ex))
-            raise web.HTTPInternalServerError(reason='Failed to create plugin configuration.')
+            except Exception as ex:
+                await config_mgr.delete_category_and_children_recursively(name)
+                _logger.exception("Failed to create plugin configuration. %s", str(ex))
+                raise web.HTTPInternalServerError(reason='Failed to create plugin configuration.')
 
         # If all successful then lastly add a schedule to run the new service at startup
         try:
@@ -277,6 +277,34 @@ async def add_service(request):
         return web.json_response({'name': name, 'id': str(schedule.schedule_id)})
 
 
+def load_python_plugin(plugin: str, service_type: str) -> Tuple[str, Dict]:
+    plugin_module_path = "foglamp.plugins.south"
+    import_file_name = "{path}.{dir}.{file}".format(path=plugin_module_path, dir=plugin, file=plugin)
+    _plugin = __import__(import_file_name, fromlist=[''])
+
+    # Fetch configuration from the configuration defined in the plugin
+    plugin_info = _plugin.plugin_info()
+    if plugin_info['type'] != service_type:
+        msg = "Plugin of {} type is not supported".format(plugin_info['type'])
+        _logger.exception(msg)
+        raise web.HTTPBadRequest(reason=msg)
+    return plugin_module_path, plugin_info
+
+
+def load_c_plugin(plugin_module_path: str, plugin: str, service_type: str) -> Dict:
+    plugin_info = apiutils.get_plugin_info(plugin, dir=service_type)
+    if plugin_info['type'] != service_type:
+        msg = "Plugin of {} type is not supported".format(plugin_info['type'])
+        _logger.exception(msg)
+        raise web.HTTPBadRequest(reason=msg)
+    plugin_config = plugin_info['config']
+    if not plugin_config:
+        # FIXME: exception message
+        _logger.exception("Plugin %s import problem from path %s. %s", plugin, plugin_module_path, str(ex))
+        raise web.HTTPNotFound(reason='Plugin "{}" import problem from path "{}".'.format(plugin, plugin_module_path))
+    return plugin_config
+
+
 async def check_scheduled_processes(storage, process_name):
     payload = PayloadBuilder().SELECT("name").WHERE(['name', '=', process_name]).payload()
     result = await storage.query_tbl_with_payload('scheduled_processes', payload)
@@ -291,5 +319,11 @@ async def check_schedules(storage, schedule_name):
 
 async def get_schedule(storage, schedule_name):
     payload = PayloadBuilder().SELECT(["id", "enabled"]).WHERE(['schedule_name', '=', schedule_name]).payload()
+    result = await storage.query_tbl_with_payload('schedules', payload)
+    return result
+
+
+async def check_notification_schedule(storage):
+    payload = PayloadBuilder().SELECT("process_name").payload()
     result = await storage.query_tbl_with_payload('schedules', payload)
     return result

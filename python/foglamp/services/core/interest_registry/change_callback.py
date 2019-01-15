3ae4ae20d893c828b68e7acf68209ac07d40fff6
Praveen Garg <praveen.garg@nerdapplabs.com>
2019-01-15 16:19:11 +0530

    removed the comments

--- python/foglamp/services/core/interest_registry/change_callback.py
+++ python/foglamp/services/core/interest_registry/change_callback.py
@@ -40,8 +40,6 @@ async def run(category_name):
     except interest_registry_exceptions.DoesNotExist:
         return
 
-    # get configuration of category_name,
-    # FIXME: :remove me: get_category_all_items should also handle type script
     category_value = await cfg_mgr.get_category_all_items(category_name)
     payload = {"category" : category_name, "items" : category_value}
     headers = {'content-type': 'application/json'}

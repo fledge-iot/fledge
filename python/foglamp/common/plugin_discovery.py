0d29696d1e008be9b21fb7b2473a6fe93ec52834
ashish-jabble <ashish@dianomic.com>
2019-01-15 13:59:52 +0530

    discovery of rule plugins added with their unit tests

--- python/foglamp/common/plugin_discovery.py
+++ python/foglamp/common/plugin_discovery.py
@@ -33,13 +33,15 @@ class PluginDiscovery(object):
             plugins_list_c_south = cls.fetch_c_plugins_installed("south", is_config)
             plugins_list_c_filter = cls.fetch_c_plugins_installed("filter", is_config)
             plugins_list_c_notify = cls.fetch_c_plugins_installed("notify", is_config)
+            plugins_list_c_rule = cls.fetch_c_plugins_installed("rule", is_config)
             plugins_list.extend(plugins_list_north)
             plugins_list.extend(plugins_list_c_north)
             plugins_list.extend(plugins_list_south)
             plugins_list.extend(plugins_list_c_south)
             plugins_list.extend(plugins_list_c_filter)
             plugins_list.extend(plugins_list_c_notify)
-        elif plugin_type == 'filter' or plugin_type == 'notify':
+            plugins_list.extend(plugins_list_c_rule)
+        elif plugin_type == 'filter' or plugin_type == 'notify' or plugin_type == 'rule':
             plugins_list = cls.fetch_c_plugins_installed(plugin_type, is_config)
         else:
             plugins_list = cls.fetch_plugins_installed(plugin_type, is_config)

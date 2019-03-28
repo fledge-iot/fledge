c31fd0dbd4546c430b4a897cc93d009bb66b0ff3
ashish-jabble <ashish@dianomic.com>
2019-03-28 15:45:00 +0530

    rule and delivery notification plugin tests added and other minor fixes

--- tests/system/python/api/test_plugin_discovery.py
+++ tests/system/python/api/test_plugin_discovery.py
@@ -32,8 +32,12 @@ def install_plugin(type, plugin, branch="develop", plugin_lang="python", use_pip
         assert False, "{} plugin installation failed".format(plugin)
 
     # Cleanup code
+    if type == "notificationDelivery":
+        type = "notify"
+    if type == "notificationRule":
+        type = "rule"
+        subprocess.run(["rm -rf /tmp/foglamp-service-notification"], shell=True, check=True)
     subprocess.run(["rm -rf /tmp/foglamp-{}-{}".format(type, plugin)], shell=True, check=True)
-    # TODO: FOGL-2642 - Remove notificationDelivery And notificationRule plugins
 
 
 @pytest.fixture
@@ -53,32 +57,42 @@ def reset_plugins():
     subprocess.run(["path=$FOGLAMP_ROOT/plugins/filter; "
                     "find $path -maxdepth 1 | grep -v \"^${path}$\" | "
                     "egrep -v '(common)' | xargs rm -rf"], shell=True, check=True)
-    # TODO: FOGL-2642 - reset notificationDelivery And notificationRule plugins
+    subprocess.run(["rm -rf $FOGLAMP_ROOT/plugins/notificationDelivery"], shell=True, check=True)
+    subprocess.run(["rm -rf $FOGLAMP_ROOT/plugins/notificationRule"], shell=True, check=True)
 
 
 class TestPluginDiscovery:
 
-    def test_default_all_plugins_installed(self, reset_plugins, reset_and_start_foglamp, foglamp_url):
+    def test_cleanup(self, reset_plugins, reset_and_start_foglamp):
+        # TODO: Remove this workaround
+        # This test to reset stuff And especially to avoid multiple fixture calls when parameterized tests
+        pass
+
+    @pytest.mark.parametrize("param, config", [
+        ("", False),
+        ("?config=false", False),
+        ("?config=true", True)
+    ])
+    def test_default_all_plugins_installed(self, foglamp_url, param, config):
         conn = http.client.HTTPConnection(foglamp_url)
-        conn.request("GET", '/foglamp/plugins/installed')
+        conn.request("GET", '/foglamp/plugins/installed{}'.format(param))
         r = conn.getresponse()
         assert 200 == r.status
         r = r.read().decode()
         jdoc = json.loads(r)
+        # Only north plugins (2 north_c and 2 north python) expected by default
         assert 4 == len(jdoc['plugins'])
         for plugin in jdoc['plugins']:
             assert 'north' == plugin['type']
-            assert 'south' not in plugin['type']
-            assert 'filter' not in plugin['type']
-            assert 'notify' not in plugin['type']
-            assert 'rule' not in plugin['type']
-            assert 'config' not in plugin
+            assert plugin['type'] not in ['south', 'filter', 'notificationDelivery', 'notificationRule']
+            # config is not expected by default
+            assert 'config' in plugin if config else 'config' not in plugin
 
     @pytest.mark.parametrize("method, count, config", [
         ("/foglamp/plugins/installed?type=south", 0, None),
         ("/foglamp/plugins/installed?type=filter", 0, None),
-        ("/foglamp/plugins/installed?type=notify", 0, None),
-        ("/foglamp/plugins/installed?type=rule", 0, None),
+        ("/foglamp/plugins/installed?type=notificationDelivery", 0, None),
+        ("/foglamp/plugins/installed?type=notificationRule", 0, None),
         ("/foglamp/plugins/installed?type=north&config=false", 4, False),
         ("/foglamp/plugins/installed?type=north&config=true", 4, True)
     ])
@@ -94,10 +108,11 @@ class TestPluginDiscovery:
         for plugin in jdoc['plugins']:
             assert 'config' in plugin if config else 'config' not in plugin
             name.append(plugin['name'])
+        # Verify only 4 north plugins when type is north
         if count == 4:
             assert Counter(['ocs', 'pi_server', 'PI_Server_V2', 'ocs_V2']) == Counter(name)
 
-    def test_south_plugins_installed(self, reset_plugins, foglamp_url):
+    def test_south_plugins_installed(self, foglamp_url):
         install_plugin(type='south', plugin='sinusoid', plugin_lang='python')
 
         conn = http.client.HTTPConnection(foglamp_url)
@@ -124,9 +139,9 @@ class TestPluginDiscovery:
         assert 'sinusoid' == plugins[0]['name']
         assert 'Random' == plugins[1]['name']
 
-    def test_north_plugins_installed(self, reset_plugins, foglamp_url):
+    def test_north_plugins_installed(self, foglamp_url):
+        # install north plugin (Python version)
         install_plugin(type='north', plugin='http', plugin_lang='python')
-
         conn = http.client.HTTPConnection(foglamp_url)
         conn.request("GET", '/foglamp/plugins/installed?type=north')
         r = conn.getresponse()
@@ -136,12 +151,13 @@ class TestPluginDiscovery:
         assert len(jdoc), "No data found"
         plugins = jdoc['plugins']
         plugin_names = [name['name'] for name in plugins]
+        # verify north plugins which is 4 by default and a new one (http) installed
         assert 5 == len(plugins)
         assert Counter(['ocs', 'http_north', 'pi_server', 'PI_Server_V2',
                         'ocs_V2']) == Counter(plugin_names)
 
-        install_plugin(type='north', plugin='thingspeak', plugin_lang='c')
-
+        # install one more north plugin (C version)
+        install_plugin(type='north', plugin='thingspeak', plugin_lang='C')
         conn.request("GET", '/foglamp/plugins/installed?type=north')
         r = conn.getresponse()
         assert 200 == r.status
@@ -150,13 +166,14 @@ class TestPluginDiscovery:
         assert len(jdoc), "No data found"
         plugins = jdoc['plugins']
         plugin_names = [name['name'] for name in plugins]
+        # verify north plugins which is 4 by default and 2 new one (Python & C version) installed
         assert 6 == len(plugins)
         assert Counter(['ocs', 'http_north', 'pi_server', 'PI_Server_V2', 'ThingSpeak',
                         'ocs_V2']) == Counter(plugin_names)
 
-    def test_filter_plugins_installed(self, reset_plugins, foglamp_url):
-        install_plugin(type='filter', plugin='rms', plugin_lang='c')
-
+    def test_filter_plugins_installed(self, foglamp_url):
+        # install rms filter plugin
+        install_plugin(type='filter', plugin='rms', plugin_lang='C')
         conn = http.client.HTTPConnection(foglamp_url)
         conn.request("GET", '/foglamp/plugins/installed?type=filter')
         r = conn.getresponse()
@@ -168,10 +185,30 @@ class TestPluginDiscovery:
         assert 1 == len(plugins)
         assert 'rms' == plugins[0]['name']
 
-    @pytest.mark.skip(reason='FOGL-2642')
-    def test_delivery_plugins_installed(self, reset_plugins, foglamp_url):
-        pass
+    def test_delivery_plugins_installed(self, foglamp_url):
+        # install slack delivery plugin
+        install_plugin(type='notificationDelivery', plugin='slack', plugin_lang='C')
+        conn = http.client.HTTPConnection(foglamp_url)
+        conn.request("GET", '/foglamp/plugins/installed?type=notificationDelivery')
+        r = conn.getresponse()
+        assert 200 == r.status
+        r = r.read().decode()
+        jdoc = json.loads(r)
+        assert len(jdoc), "No data found"
+        plugins = jdoc['plugins']
+        assert 1 == len(plugins)
+        assert 'slack' == plugins[0]['name']
 
-    @pytest.mark.skip(reason='FOGL-2642')
-    def test_rule_plugins_installed(self, reset_plugins, foglamp_url):
-        pass
+    def test_rule_plugins_installed(self, foglamp_url):
+        # install outofbound rule plugin
+        install_plugin(type='notificationRule', plugin='outofbound', plugin_lang='C')
+        conn = http.client.HTTPConnection(foglamp_url)
+        conn.request("GET", '/foglamp/plugins/installed?type=notificationRule')
+        r = conn.getresponse()
+        assert 200 == r.status
+        r = r.read().decode()
+        jdoc = json.loads(r)
+        assert len(jdoc), "No data found"
+        plugins = jdoc['plugins']
+        assert 1 == len(plugins)
+        assert 'OutOfBound' == plugins[0]['name']

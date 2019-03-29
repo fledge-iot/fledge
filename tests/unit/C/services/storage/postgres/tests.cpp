4efcef606e021d4fb29266469c194b2e6aa0a827
stefano <stefano@dianomic.com>
2019-03-29 16:36:02 +0100

    FOGL-2650: repeat to 1000

--- tests/unit/C/services/storage/postgres/tests.cpp
+++ tests/unit/C/services/storage/postgres/tests.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
 
 	testing::InitGoogleTest(&argc, argv);
 
-	testing::GTEST_FLAG(repeat) = 5000;
+	testing::GTEST_FLAG(repeat) = 1000;
 	testing::GTEST_FLAG(shuffle) = true;
 	testing::GTEST_FLAG(break_on_failure) = true;
 

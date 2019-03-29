d93e0d6f4bab9891ab773a645ad9627c8f7874c8
stefano <stefano@dianomic.com>
2019-03-29 14:15:01 +0100

    FOGL-2650: Select the proper storage.json file for the tests.

--- tests/unit/C/services/storage/postgres/tests.cpp
+++ tests/unit/C/services/storage/postgres/tests.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
 
-    testing::GTEST_FLAG(repeat) = 5000;
+    testing::GTEST_FLAG(repeat) = 1;
     testing::GTEST_FLAG(shuffle) = true;
     testing::GTEST_FLAG(break_on_failure) = true;
 
@@ -16,11 +16,25 @@ int main(int argc, char **argv) {
 }
 
 /**
+ * Select the proper storage.json file for the tests
+ */
+void cache_file_select()
+{
+
+	string foglamp_root = getenv("FOGLAMP_ROOT");
+	string foglamp_data = foglamp_root + "/tests/unit/C/services/storage/postgres";
+
+	char buf[512];
+	snprintf(buf, sizeof(buf), "FOGLAMP_DATA=%s", foglamp_data.c_str());
+	putenv(buf);
+}
+
+/**
  * Test retrieval of port from default
  */
 TEST(ConfigurationTest, getport)
 {
-StorageConfiguration	conf;
+	StorageConfiguration	conf;
 
 	ASSERT_EQ(strcmp(conf.getValue(string("port")), "0"), 0);
 }
@@ -30,13 +44,11 @@ StorageConfiguration	conf;
  */
 TEST(ConfigurationTest, getplugin)
 {
-StorageConfiguration	conf;
+	cache_file_select();
 
-	// This test cannot be executed in the automated system
-	// Uncomment locally
-	//ASSERT_EQ(strcmp(conf.getValue(string("plugin")), "postgres"), 0);
-	// Uncomment on Remote
-	ASSERT_EQ(0, 0);
+	StorageConfiguration	conf;
+
+	ASSERT_EQ(strcmp(conf.getValue(string("plugin")), "postgres"), 0);
 }
 
 /**
@@ -44,9 +56,8 @@ StorageConfiguration	conf;
  */
 TEST(ConfigurationTest, setport)
 {
-StorageConfiguration	conf;
+	StorageConfiguration	conf;
 
-	
 	ASSERT_EQ(true, conf.setValue(string("port"), string("8188")));
 	ASSERT_EQ(strcmp(conf.getValue(string("port")), "8188"), 0);
 }

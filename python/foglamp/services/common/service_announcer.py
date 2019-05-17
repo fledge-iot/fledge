b78869ee4c8d54972ebde74bf9d13ed9ef522d5b
amarendra-dianomic <amarendra@dianomic.com>
2019-05-17 18:13:18 +0530

    FOGL-2641 - Service announcer modified to use zeroconf lib instead avahi/dbus lib

--- python/foglamp/services/common/service_announcer.py
+++ python/foglamp/services/common/service_announcer.py
@@ -6,52 +6,49 @@
 
 """Common FoglampMicroservice Class"""
 
-import foglamp.services.common.avahi as avahi
-from foglamp.common import logger
-import dbus
 
-__author__ = "Mark Riddoch"
+import socket
+from zeroconf import ServiceInfo, Zeroconf
+
+from foglamp.common import logger
+
+
+__author__ = "Mark Riddoch, Amarendra K Sinha"
 __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
 __license__ = "Apache 2.0"
 __version__ = "${VERSION}"
 
 _LOGGER = logger.setup(__name__)
 
+
 class ServiceAnnouncer:
-    _service_name = None
-    """ The name of the service to advertise """
-
-    _group = None
-    """ The Avahi group """
-
-    def __init__(self, name, service, port, txt):
-        try:
-            bus = dbus.SystemBus()
-            server = dbus.Interface(bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
-            self._group = dbus.Interface(bus.get_object(avahi.DBUS_NAME, server.EntryGroupNew()),
-                                     avahi.DBUS_INTERFACE_ENTRY_GROUP)
-
-            self._service_name = name
-            index = 1
-            while True:
-                try:
-                    self._group.AddService(avahi.IF_UNSPEC, avahi.PROTO_INET, 0, self._service_name, service, '', '', port,
-                                       avahi.string_array_to_txt_array(txt))
-                except dbus.DBusException:  # name collision -> rename
-                    index += 1
-                    self._service_name = '%s #%s' % (name, str(index))
-                else:
-                    break
-
-            self._group.Commit()
-        except Exception:
-            _LOGGER.error("Avahi not available, continuing without service discovery available")
-
-    @property
-    def get_service_name(self):
-        return self._service_name
-
-    def unregister(self):
-        if self._group is not None:
-            self._group.Reset()
-            self._group = None
+    def __init__(self, sname, stype, port, txt):
+        host_name = socket.gethostname()
+        host = socket.gethostbyname(host_name)
+        service_name = "_" + sname.lower() + "._tcp.local."
+        desc = {'path': '/~paulsm/'}  # TODO: Change
+        _LOGGER.error(">>>>>>>>>>>>>>>>> %s", service_name)
+        """Create a service description.
+                type_: fully qualified service type name
+                name: fully qualified service name
+                address: IP address as unsigned short, network byte order
+                port: port that the service runs on
+                weight: weight of the service
+                priority: priority of the service
+                properties: dictionary of properties (or a string holding the
+                            bytes for the text field)
+                server: fully qualified name for service host (defaults to name)
+                host_ttl: ttl used for A/SRV records
+                other_ttl: ttl used for PTR/TXT records"""
+        info = ServiceInfo(
+            "_foglamp-manage._tcp.local.",
+            host_name + "._foglamp-manage._tcp.local.",
+            socket.inet_aton(host),
+            port,
+            0,
+            0,
+            desc,
+            "foglamp.local.",
+        )
+        zeroconf = Zeroconf()
+        zeroconf.register_service(info)

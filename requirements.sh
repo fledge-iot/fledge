46fdca42b09d80d4977023e502f54ebc819ca78e
Stefano Simonelli <stefano@dianomic.com>
2019-06-14 16:26:27 +0200

    FOGL-2808: testing

--- requirements.sh
+++ requirements.sh
@@ -63,6 +63,7 @@ if [[ ( $os_name == *"Red Hat"* || $os_name == *"CentOS"* ) &&  $os_version == *
 	echo "Compiling SQLite3 static library for FogLAMP ..."
 	./configure --enable-shared=false --enable-static=true --enable-static-shell CFLAGS="-DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_LOAD_EXTENSION -DSQLITE_ENABLE_COLUMN_METADATA -fno-common -fPIC"
 	autoreconf -f -i
+	set +e
 	make
 	exit_code=$?
 	if [[ $exit_code != 0 ]]; then
@@ -70,6 +71,7 @@ if [[ ( $os_name == *"Red Hat"* || $os_name == *"CentOS"* ) &&  $os_version == *
 		make
 	fi
 	cd $foglamp_location
+	set -e
 
 	# To avoid to stop the execution for any internal error of scl_source
 	set +e

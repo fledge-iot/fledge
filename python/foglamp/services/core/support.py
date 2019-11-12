b3c5b548c1cf0b6cf2961a38995c1d60bbfc0f24
ashish-jabble <ashish@dianomic.com>
2019-11-12 19:13:08 +0530

    support bundle psinfo fixes

--- python/foglamp/services/core/support.py
+++ python/foglamp/services/core/support.py
@@ -175,11 +175,16 @@ class SupportBuilder:
     def add_psinfo(self, pyz, file_spec):
         # A PS listing of al the python applications running on the machine
         temp_file = self._interim_file_path + "/" + "psinfo-{}".format(file_spec)
-        a = subprocess.Popen(
-            'ps -eaf | grep python3', shell=True, stdout=subprocess.PIPE).stdout.readlines()[:-2]  # remove ps command
+        a = subprocess.Popen('ps -eaf | grep "foglamp\."', shell=True,
+                             stdout=subprocess.PIPE).stdout.readlines()
         c = [b.decode() for b in a]  # Since "a" contains return value in bytes, convert it to string
+
+        c_tasks = subprocess.Popen('ps -eaf | grep "./tasks/sending_process"', shell=True, stdout=subprocess.PIPE).stdout.readlines()
+        c_tasks_decode = [t.decode() for t in c_tasks]
+        if c_tasks_decode:
+            c.extend(c_tasks_decode)
         data = {
-            "runningPythonProcesses": c
+            "runningProcesses": c
         }
         self.write_to_tar(pyz, temp_file, data)
 

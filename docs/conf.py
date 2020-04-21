51c4bbe2ef4089277a7e8fd93d5ba5d37ed5e2a9
Mark Riddoch <mark@dianomic.com>
2020-04-21 11:29:25 +0000

    FOGL-3965 Add documentation for notification service
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Fledge'
-copyright = u'2018, Dianomic Systems'
+copyright = u'2020, Dianomic Systems'
 author = u'Dianomic Systems'
 
 # The version info for the project you're documenting, acts as replacement for

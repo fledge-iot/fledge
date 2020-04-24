e97ba322d3a87cc2bf74e8b0fe2f09b2a9f62f55
Mark Riddoch <mark@dianomic.com>
2020-04-24 11:13:06 +0000

    Removed fledge_pugins.rst as it is now generated always
    Combine make targets
    
    Add remove of plugisn to make clean
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- docs/conf.py
+++ docs/conf.py
@@ -174,5 +174,4 @@ html_context = {
         ],
      }
 
-subprocess.run(["make fledge_plugins.rst"], shell=True, check=True)
-subprocess.run(["make plugins"], shell=True, check=True)
+subprocess.run(["make generated"], shell=True, check=True)

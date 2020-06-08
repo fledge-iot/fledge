35f52036e1dc7e4ca2e2a14e8bf793628c894dee
Stefano <stefano@dianomic.com>
2020-06-08 13:08:36 +0000

    FOGL-3908: fixed init script
    Signed-off-by: Stefano <stefano@dianomic.com>

--- scripts/plugins/storage/sqlite/init.sql
+++ scripts/plugins/storage/sqlite/init.sql
@@ -209,29 +209,6 @@ CREATE INDEX fki_asset_messages_fk1
 CREATE INDEX fki_asset_messages_fk2
     ON asset_messages (status_id);
 
---// FIXME_I:
--- -- Readings table
--- -- This tables contains the readings for assets.
--- -- An asset can be a south with multiple sensor, a single sensor,
--- -- a software or anything that generates data that is sent to Fledge
--- CREATE TABLE fledge.readings (
---     id         INTEGER                     PRIMARY KEY AUTOINCREMENT,
---     asset_code character varying(50)       NOT NULL,                         -- The provided asset code. Not necessarily located in the
---                                                                              -- assets table.
---     reading    JSON                        NOT NULL DEFAULT '{}',            -- The json object received
---     user_ts    DATETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f+00:00', 'NOW')),      -- UTC time
---     ts         DATETIME DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f+00:00', 'NOW'))       -- UTC time
--- );
---
--- CREATE INDEX fki_readings_fk1
---     ON readings (asset_code, user_ts desc);
---
--- CREATE INDEX readings_ix2
---     ON readings (asset_code);
---
--- CREATE INDEX readings_ix3
---     ON readings (user_ts);
-
 -- Streams table
 -- List of the streams to the Cloud.
 CREATE TABLE fledge.streams (

c641eba6d1a6f7e425e01ba59ed66a4e26daceb8
YashTatkondawar <yashtatkondawar@gmail.com>
2021-06-22 18:41:40 +0530

    System tests with filters in OMF north service
    Signed-off-by: YashTatkondawar <yashtatkondawar@gmail.com>

--- tests/system/python/packages/test_omf_north_service.py
+++ tests/system/python/packages/test_omf_north_service.py
@@ -26,6 +26,7 @@ south_service_name="Sine #1"
 north_plugin = "OMF"
 north_service_name="NorthReadingsToPI_WebAPI"
 north_schedule_id=""
+filter1_name="SF1"
 # This  gives the path of directory where fledge is cloned. test_file < packages < python < system < tests < ROOT
 PROJECT_ROOT = Path(__file__).parent.parent.parent.parent.parent
 SCRIPTS_DIR_ROOT = "{}/tests/system/python/scripts/package/".format(PROJECT_ROOT)
@@ -56,6 +57,14 @@ def start_south_north(add_south, start_north_pi_server_c_web_api_service, fledge
     north_schedule_id = response["id"]
 
     yield start_south_north
+
+
+@pytest.fixture
+def add_configure_filter(add_filter, fledge_url):    
+    filter_cfg_scale = {"enable": "true"}
+    add_filter("scale", None, filter1_name, filter_cfg_scale, fledge_url, north_service_name, installation_type='package')
+
+    yield add_configure_filter
 
 
 def verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries):
@@ -99,11 +108,23 @@ def verify_service_added(fledge_url):
     assert len(result["services"])
     assert south_service_name in [s["name"] for s in result["services"]]
     
+    get_url = "/fledge/north"
+    result = utils.get_request(fledge_url, get_url)
+    assert len(result)
+    assert north_service_name in [s["name"] for s in result]
+    
     get_url = "/fledge/service"
     result = utils.get_request(fledge_url, get_url)
     assert len(result["services"])
     assert south_service_name in [s["name"] for s in result["services"]]
     assert north_service_name in [s["name"] for s in result["services"]]
+
+
+def verify_filter_added(fledge_url):
+    get_url = "/fledge/filter"
+    result = utils.get_request(fledge_url, get_url)
+    assert len(result["filters"])
+    assert filter1_name in [s["name"] for s in result["filters"]]
 
 
 def verify_asset(fledge_url):
@@ -300,5 +321,214 @@ class TestOMFNorthService:
         new_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
         # Verifies whether Read and Sent readings are increasing after delete/add of north service
         assert old_ping_result['dataRead'] < new_ping_result['dataRead']
-        assert old_ping_result['dataSent'] == new_ping_result['dataSent']        
+        assert old_ping_result['dataSent'] == new_ping_result['dataSent']
+
+
+class TestOMFNorthServicewithFilters:
+    def test_omf_service_with_filter(self, reset_fledge, start_south_north, add_configure_filter, skip_verify_north_interface, fledge_url, wait_time, retries):
+        """ Test OMF as a North service with filters.
+            remove_and_add_pkgs: Fixture to remove and install latest fledge packages
+            reset_fledge: Fixture to reset fledge
+            start_south_north: Adds and configures south(sinusoid) and north(OMF) service
+            Assertions:
+                on endpoint GET /fledge/south
+                on endpoint GET /fledge/ping
+                on endpoint GET /fledge/asset"""        
         
+        # Wait until south and north services are created
+        time.sleep(wait_time)
+        
+        old_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)        
+        verify_asset(fledge_url)
+        verify_service_added(fledge_url)
+        verify_filter_added(fledge_url)        
+        verify_statistics_map(fledge_url, skip_verify_north_interface)
+        
+        # Wait for read and sent readings to increase
+        time.sleep(wait_time)
+        
+        new_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        # Verifies whether Read and Sent readings are increasing after delete/add of north service
+        assert old_ping_result['dataRead'] < new_ping_result['dataRead']
+        assert old_ping_result['dataSent'] < new_ping_result['dataSent']
+        
+    def test_omf_service_with_disable_enable_filter(self, reset_fledge, start_south_north, add_configure_filter, skip_verify_north_interface, fledge_url, wait_time, retries):
+        """ Test OMF as a North service with enable/diable of filters.
+            remove_and_add_pkgs: Fixture to remove and install latest fledge packages
+            reset_fledge: Fixture to reset fledge
+            start_south_north: Adds and configures south(sinusoid) and north(OMF) service
+            Assertions:
+                on endpoint GET /fledge/south
+                on endpoint GET /fledge/ping
+                on endpoint GET /fledge/asset"""        
+        
+        # Wait until south and north services are created
+        time.sleep(wait_time)
+        
+        verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)        
+        verify_asset(fledge_url)
+        verify_service_added(fledge_url)
+        verify_filter_added(fledge_url)    
+        verify_statistics_map(fledge_url, skip_verify_north_interface)
+        
+        data = {"enable": "false"}
+        put_url = "/fledge/category/{}_SF1".format(north_service_name)
+        resp = utils.put_request(fledge_url, urllib.parse.quote(put_url), data)
+        assert "false" == resp['enable']['value']
+        
+        # Wait for service to disable
+        time.sleep(wait_time)        
+        
+        data = {"enable": "true"}
+        put_url = "/fledge/category/{}_SF1".format(north_service_name)
+        resp = utils.put_request(fledge_url, urllib.parse.quote(put_url), data)
+        assert "true" == resp['enable']['value']
+        
+        old_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        
+        # Wait for read and sent readings to increase
+        time.sleep(wait_time)
+        
+        new_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        # Verifies whether Read and Sent readings are increasing after disable/enable
+        assert old_ping_result['dataRead'] < new_ping_result['dataRead']
+        assert old_ping_result['dataSent'] < new_ping_result['dataSent']
+        
+    def test_omf_service_with_filter_reconfig(self, reset_fledge, start_south_north, add_configure_filter, skip_verify_north_interface, fledge_url, wait_time, retries):
+        """ Test OMF as a North service with reconfiguration of filters.
+            remove_and_add_pkgs: Fixture to remove and install latest fledge packages
+            reset_fledge: Fixture to reset fledge
+            start_south_north: Adds and configures south(sinusoid) and north(OMF) service
+            Assertions:
+                on endpoint GET /fledge/south
+                on endpoint GET /fledge/ping
+                on endpoint GET /fledge/asset"""        
+        
+        # Wait until south and north services are created
+        time.sleep(wait_time)
+        
+        verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)        
+        verify_asset(fledge_url)
+        verify_service_added(fledge_url)
+        verify_filter_added(fledge_url)      
+        verify_statistics_map(fledge_url, skip_verify_north_interface)
+        
+        data = {"factor": "50"}
+        put_url = "/fledge/category/{}_SF1".format(north_service_name)
+        resp = utils.put_request(fledge_url, urllib.parse.quote(put_url), data)
+        assert "50.0" == resp['factor']['value']
+        
+        # Wait for filter reconfiguration
+        time.sleep(wait_time)        
+        
+        old_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        
+        # Wait for read and sent readings to increase
+        time.sleep(wait_time)
+        
+        new_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        # Verifies whether Read and Sent readings are increasing after disable/enable
+        assert old_ping_result['dataRead'] < new_ping_result['dataRead']
+        assert old_ping_result['dataSent'] < new_ping_result['dataSent']
+        
+    @pytest.mark.skip(reason="FOGL-5215: Deleting a service doesn't delete its filter categories")
+    def test_omf_service_with_delete_add(self, reset_fledge, start_south_north, add_configure_filter, add_filter, start_north_pi_server_c_web_api_service, skip_verify_north_interface, fledge_url, wait_time, retries, pi_host, pi_port, pi_admin, pi_passwd):
+        """ Test OMF as a North service by deleting and adding north service.
+            remove_and_add_pkgs: Fixture to remove and install latest fledge packages
+            reset_fledge: Fixture to reset fledge
+            start_south_north: Adds and configures south(sinusoid) and north(OMF) service
+            Assertions:
+                on endpoint GET /fledge/south
+                on endpoint GET /fledge/ping
+                on endpoint GET /fledge/asset"""               
+        
+        global north_schedule_id
+        
+        # Wait until south and north services are created
+        time.sleep(wait_time)
+        
+        verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)        
+        verify_asset(fledge_url)
+        verify_service_added(fledge_url)      
+        verify_filter_added(fledge_url)  
+        verify_statistics_map(fledge_url, skip_verify_north_interface)
+        
+        delete_url = "/fledge/service/{}".format(north_service_name)
+        resp = utils.delete_request(fledge_url, delete_url)
+        assert "Service {} deleted successfully.".format(north_service_name) == resp['result']
+        
+        # Wait for service to get deleted
+        time.sleep(wait_time)
+    
+        response = start_north_pi_server_c_web_api_service(fledge_url, pi_host, pi_port, pi_user=pi_admin, pi_pwd=pi_passwd)
+        north_schedule_id = response["id"]
+        
+        filter_cfg_scale = {"enable": "true"}
+        add_filter("scale", None, "SF2", filter_cfg_scale, fledge_url, north_service_name, installation_type='package')
+        
+        # Wait for service and filter to get added
+        time.sleep(wait_time)
+        
+        verify_service_added(fledge_url)      
+        verify_filter_added(fledge_url)
+        
+        old_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        
+        # Wait for read and sent readings to increase
+        time.sleep(wait_time)
+        
+        new_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        # Verifies whether Read and Sent readings are increasing after delete/add of north service
+        assert old_ping_result['dataRead'] < new_ping_result['dataRead']
+        assert old_ping_result['dataSent'] < new_ping_result['dataSent']
+        
+    
+    def test_omf_service_with_delete_add_filter(self, reset_fledge, start_south_north, add_configure_filter, add_filter, skip_verify_north_interface, fledge_url, wait_time, retries, pi_host, pi_port, pi_admin, pi_passwd):
+        """ Test OMF as a North service by deleting and adding north service.
+            remove_and_add_pkgs: Fixture to remove and install latest fledge packages
+            reset_fledge: Fixture to reset fledge
+            start_south_north: Adds and configures south(sinusoid) and north(OMF) service
+            Assertions:
+                on endpoint GET /fledge/south
+                on endpoint GET /fledge/ping
+                on endpoint GET /fledge/asset"""       
+        
+        # Wait until south and north services are created
+        time.sleep(wait_time)
+        
+        verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)        
+        verify_asset(fledge_url)
+        verify_service_added(fledge_url)      
+        verify_filter_added(fledge_url)  
+        verify_statistics_map(fledge_url, skip_verify_north_interface)
+        
+        data = {"pipeline": []}
+        put_url = "/fledge/filter/{}/pipeline?allow_duplicates=true&append_filter=false" \
+            .format(north_service_name)
+        resp = utils.put_request(fledge_url, urllib.parse.quote(put_url, safe='?,=,&,/'), data)
+        
+        delete_url = "/fledge/filter/{}".format(filter1_name)
+        resp = utils.delete_request(fledge_url, delete_url)
+        print (resp)
+        assert "Filter {} deleted successfully".format(filter1_name) == resp['result']
+        
+        # Wait for service to get deleted
+        time.sleep(wait_time)
+        
+        filter_cfg_scale = {"enable": "true"}
+        add_filter("scale", None, filter1_name, filter_cfg_scale, fledge_url, north_service_name, installation_type='package')
+        
+        # Wait for filter to get added
+        time.sleep(wait_time)
+      
+        verify_filter_added(fledge_url)
+        
+        old_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        
+        # Wait for read and sent readings to increase
+        time.sleep(wait_time)
+        
+        new_ping_result = verify_ping(fledge_url, skip_verify_north_interface, wait_time, retries)
+        # Verifies whether Read and Sent readings are increasing after delete/add of north service
+        assert old_ping_result['dataRead'] < new_ping_result['dataRead']
+        assert old_ping_result['dataSent'] < new_ping_result['dataSent']

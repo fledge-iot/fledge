56087cc4fe4d2faa6789816b39dbd807e6c8cce1
Mark Riddoch <mark@dianomic.com>
2021-07-28 15:20:36 +0000

    FOGL-5704 escape quotes in JSON readings
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/common/include/datapoint.h
+++ C/common/include/datapoint.h
@@ -175,6 +175,7 @@ class DatapointValue {
 
 	private:
 		void deleteNestedDPV();
+		const std::string	escape(const std::string& str) const;
 		union data_t {
 			std::string*		str;
 			long			i;
--- C/common/reading.cpp
+++ C/common/reading.cpp
@@ -174,7 +174,7 @@ string Reading::toJSON(bool minimal) const
 ostringstream convert;
 
 	convert << "{\"asset_code\":\"";
-	convert << m_asset;
+	convert << escape(m_asset);
 	convert << "\",\"user_ts\":\"";
 
 	// Add date_time with microseconds + timezone UTC:
@@ -393,5 +393,39 @@ void Reading::stringToTimestamp(const string& timestamp, struct timeval *ts)
 		ptr++;
 		sscanf(ptr, "%02d:%02d", &h, &m);
 		ts->tv_sec += sign * ((3600 * h) + (60 * m));
+	}
+}
+
+/**
+ * Escape quotes etc to allow the string to be a property value within
+ * a JSON document
+ *
+ * @param str	The string to escape
+ * @return The escaped string
+ */
+const string Reading::escape(const string& str) const
+{
+string rval;
+int bscount = 0;
+
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (str[i] == '\\')
+		{
+			bscount++;
+		}
+		else if (str[i] == '\"')
+		{
+			if ((bscount & 1) == 0)	// not already escaped
+			{
+				rval += "\\";	// Add escape of "
+			}
+			bscount = 0;
+		}
+		else
+		{
+			bscount = 0;
+		}
+		rval += str[i];
 	}
 }

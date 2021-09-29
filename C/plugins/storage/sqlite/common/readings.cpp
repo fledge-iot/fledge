f45ae3519cbaf3d9914848c4182b92d26d873b81
Stefano <stefano@dianomic.com>
2021-09-29 16:01:28 +0200

    fix a bug in the purge by age using multiple readings
    Signed-off-by: Stefano <stefano@dianomic.com>

--- C/plugins/storage/sqlite/common/readings.cpp
+++ C/plugins/storage/sqlite/common/readings.cpp
@@ -2068,6 +2068,7 @@ vector<string>  assetCodes;
 		SQLBuffer sql;
 		sql.append("DELETE FROM  _dbname_._tablename_ WHERE rowid <= ");
 		sql.append(rowidMin);
+		sql.append(" AND user_ts < datetime('now' , '-" +to_string(age) + " hours')");
 		sql.append(';');
 		const char *query = sql.coalesce();
 

7d7b543a49fccacc1fca00013125ef56a1949d06
Stefano <stefano@dianomic.com>
2021-10-28 11:41:59 +0200

    pg: upgrade/downgrade
    Signed-off-by: Stefano <stefano@dianomic.com>

--- scripts/plugins/storage/sqlite/downgrade/43.sql
+++ scripts/plugins/storage/sqlite/downgrade/43.sql
@@ -6,3 +6,8 @@ UPDATE fledge.configuration SET value = json_set(value, '$.retainUnsent', json('
 UPDATE fledge.configuration SET value = json_set(value, '$.retainUnsent', json('{"description": "Retain data that has not been sent to any historian yet.", "type": "boolean",  "default": "false", "displayName": "Retain Unsent Data", "value": "true"}'))
         WHERE key = 'PURGE_READ' AND
                json_extract(value, '$.retainUnsent.value')  = "retain unsent to all destinations";
+
+UPDATE fledge.configuration SET value = json_set(value, '$.retainUnsent', json('{"description": "Retain data that has not been sent to any historian yet.", "type": "boolean",  "default": "false", "displayName": "Retain Unsent Data", "value": "true"}'))
+        WHERE key = 'PURGE_READ' AND
+               json_extract(value, '$.retainUnsent.value')  = "retain unsent to any destination";
+

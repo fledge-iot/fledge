fad63a451cff081a67d545fa42c7f3922f2f0464
Massimiliano Pinto <massimiliano.pinto@gmail.com>
2022-02-21 10:01:07 +0100

    FOGL-5144: addition of isRunning() routine
    FOGL-5144: addition of isRunning() routine

--- C/services/common/include/service_handler.h
+++ C/services/common/include/service_handler.h
@@ -21,5 +21,6 @@ class ServiceHandler
 	public:
 		virtual void	shutdown() = 0;
 		virtual void	configChange(const std::string& category, const std::string& config) = 0;
+		virtual bool	isRunning() = 0;
 };
 #endif
--- C/services/north/include/north_service.h
+++ C/services/north/include/north_service.h
@@ -38,6 +38,7 @@ class NorthService : public ServiceHandler {
 						      unsigned short corePort);
 		void 				stop();
 		void				shutdown();
+		bool				isRunning() { return !m_shutdown; };
 		void				configChange(const std::string&,
 						const std::string&);
 		static ManagementClient *	getMgmtClient();

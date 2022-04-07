b4381cf60c3fbe6a79ce6c527d27042521c9f2c8
ashish-jabble <ashish@dianomic.com>
2022-04-07 17:49:53 +0530

    removal of hardcoded service name in public proxy API's and improved exception handling a bit more
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- python/fledge/services/core/api/proxy.py
+++ python/fledge/services/core/api/proxy.py
@@ -9,7 +9,6 @@ import aiohttp
 
 from aiohttp import web
 from fledge.common import logger
-from fledge.common.service_record import ServiceRecord
 from fledge.services.core import server
 from fledge.services.core.service_registry import exceptions as service_registry_exceptions
 from fledge.services.core.service_registry.service_registry import ServiceRegistry
@@ -22,9 +21,6 @@ __version__ = "${VERSION}"
 
 
 _logger = logger.setup(__name__, level=logging.INFO)
-
-# TODO: dynamic service name or configurable
-SVC_NAME = "Bucket Storage"
 SVC_TYPE = "BucketStorage"
 
 
@@ -33,39 +29,39 @@ def setup(app):
 
 
 async def handler(request):
-    """For example: For a bucketstorage service
+    """ For example: For a bucket service
         POST -   /bucket
-        POST -   /bucket/uid
-        GET -    /bucket/uid
-        DELETE - /bucket/uid
-        PUT -    /bucket/uid
+        GET -    /bucket/([0-9][0-9]*)$
+        DELETE - /bucket/([0-9][0-9]*)$
+        PUT -    /bucket/([0-9][0-9]*)$
         PUT -    /bucket/match
     """
     allow_methods = ["GET", "POST", "PUT", "DELETE"]
     if request.method not in allow_methods:
         raise web.HTTPMethodNotAllowed(method=request.method, allowed_methods=allow_methods)
-    if SVC_NAME not in server.Server._API_PROXIES.keys():
-        raise web.HTTPForbidden()
-    # TODO: Match request.url with server.Server._PROXY_API_INFO KV pair and forward
     try:
         data = await request.json() if request.method != 'GET' else None
         svc, token = await _get_service_record_info_along_with_bearer_token()
+        if svc._name not in server.Server._API_PROXIES.keys():
+            raise web.HTTPForbidden()
         url = str(request.url).split('fledge/')[1]
-        result = await _call_microservice_service_api(request.method, svc._protocol, svc._address,
+        result = await _call_microservice_service_api(request.method, svc._protocol, svc._address, 
                                                       svc._port, url, token, data)
     except Exception as ex:
         msg = str(ex)
         raise web.HTTPInternalServerError(reason=msg, body=json.dumps({"message": msg}))
     else:
-        return web.json_response({"message": result})
+        return web.json_response({"info": result})
 
 
 async def _get_service_record_info_along_with_bearer_token():
     try:
-        service = ServiceRegistry.filter_by_name_and_type(name=SVC_NAME, s_type=SVC_TYPE)
-        token = ServiceRegistry.getBearerToken(SVC_NAME)
+        service = ServiceRegistry.get(s_type=SVC_TYPE)
+        svc_name = service[0]._name
+        token = ServiceRegistry.getBearerToken(svc_name)
     except service_registry_exceptions.DoesNotExist:
-        raise web.HTTPNotFound(reason="No {} service available.".format(SVC_NAME))
+        msg = "No service available with {} type.".format(SVC_TYPE)
+        raise web.HTTPNotFound(reason=msg, body=json.dumps({"message": msg}))
     else:
         return service[0], token
 
@@ -76,41 +72,39 @@ async def _call_microservice_service_api(method: str, protocol: str, address: st
     if token is not None:
         headers['Authorization'] = "Bearer {}".format(token)
     url = "{}://{}:{}/{}".format(protocol, address, port, uri)
-    response = {}
     try:
         if method == 'GET':
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, headers=headers) as resp:
-                    status_code = resp.status
                     response = await resp.text()
-                    if status_code not in range(200, 209):
-                        raise Exception
+                    if resp.status not in range(200, 209):
+                        raise Exception("Error: Http status code: {}, reason: {}, response: {}".format(
+                            resp.status, resp.reason, response))
         elif method == 'POST':
             async with aiohttp.ClientSession() as session:
                 async with session.post(url, data=json.dumps(data), headers=headers) as resp:
-                    status_code = resp.status
                     response = await resp.text()
-                    if status_code not in range(200, 209):
-                        raise Exception
+                    if resp.status not in range(200, 209):
+                        raise Exception("Error: Http status code: {}, reason: {}, response: {}".format(
+                            resp.status, resp.reason, response))
         elif method == 'PUT':
             async with aiohttp.ClientSession() as session:
                 async with session.put(url, data=json.dumps(data), headers=headers) as resp:
-                    status_code = resp.status
                     response = await resp.text()
-                    if status_code not in range(200, 209):
-                        raise Exception
+                    if resp.status not in range(200, 209):
+                        raise Exception("Error: Http status code: {}, reason: {}, response: {}".format(
+                            resp.status, resp.reason, response))
         elif method == 'DELETE':
             async with aiohttp.ClientSession() as session:
                 async with session.delete(url, data=json.dumps(data), headers=headers) as resp:
-                    status_code = resp.status
                     response = await resp.text()
-                    if status_code not in range(200, 209):
-                        raise Exception
+                    if resp.status not in range(200, 209):
+                        raise Exception("Error: Http status code: {}, reason: {}, response: {}".format(
+                            resp.status, resp.reason, response))
         else:
             _logger.warning("Not implemented yet for {} method.".format(method))
-    except Exception:
-        _logger.error("Error code: {}, reason: {}, details: {}, url: {}".format(
-            int(resp.status), resp.reason, response, url))
-        raise
+    except Exception as ex:
+        _logger.error(str(ex))
+        raise Exception(response)
     else:
         return response

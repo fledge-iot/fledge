192007f0c6c031311cdae54da84ac1bcaf7f9bfc
Ashish Jabble <ashish@dianomic.com>
2022-07-18 15:29:39 +0530

    plugin data API improvements (#731)
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- python/fledge/services/core/routes.py
+++ python/fledge/services/core/routes.py
@@ -24,7 +24,7 @@ from fledge.services.core.api import south
 from fledge.services.core.api import north
 from fledge.services.core.api import filters
 from fledge.services.core.api import notification
-from fledge.services.core.api import plugin_data
+from fledge.services.core.api.plugins import data as plugin_data
 from fledge.services.core.api.plugins import install as plugins_install, discovery as plugins_discovery
 from fledge.services.core.api.plugins import update as plugins_update
 from fledge.services.core.api.plugins import remove as plugins_remove
@@ -137,11 +137,6 @@ def setup(app):
     # asset tracker
     app.router.add_route('GET', '/fledge/track', asset_tracker.get_asset_tracker_events)
 
-    # plugin data
-    app.router.add_route('GET', '/fledge/service/{service_name}/plugin/{plugin_name}/data', plugin_data.get_plugin_data)
-    app.router.add_route('POST', '/fledge/service/{service_name}/plugin/{plugin_name}/data', plugin_data.add_plugin_data)
-    app.router.add_route('DELETE', '/fledge/service/{service_name}/plugin/{plugin_name}/data', plugin_data.delete_plugin_data)
-
     # Statistics - As per doc
     app.router.add_route('GET', '/fledge/statistics', api_statistics.get_statistics)
     app.router.add_route('GET', '/fledge/statistics/history', api_statistics.get_statistics_history)
@@ -190,6 +185,11 @@ def setup(app):
     app.router.add_route('POST', '/fledge/plugins', plugins_install.add_plugin)
     app.router.add_route('PUT', '/fledge/plugins/{type}/{name}/update', plugins_update.update_plugin)
     app.router.add_route('DELETE', '/fledge/plugins/{type}/{name}', plugins_remove.remove_plugin)
+
+    # plugin data
+    app.router.add_route('GET', '/fledge/service/{service_name}/plugin/{plugin_name}/data', plugin_data.get)
+    app.router.add_route('POST', '/fledge/service/{service_name}/plugin/{plugin_name}/data', plugin_data.add)
+    app.router.add_route('DELETE', '/fledge/service/{service_name}/plugin/{plugin_name}/data', plugin_data.delete)
 
     # Filters 
     app.router.add_route('POST', '/fledge/filter', filters.create_filter)

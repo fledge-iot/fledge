70b390fb99381caf6a69e0927707e72ae61e2a4a
Deepanshu Yadav <rohantheyadav@gmail.com>
2022-07-20 19:49:12 +0530

    initial code added in configuration mangement that will call respective method in aclmangement class
    Signed-off-by: Deepanshu Yadav <rohantheyadav@gmail.com>

---
+++ python/fledge/common/acl_management.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+class ACLManagement:
+    def __init__(self):
+        pass
+
+    async def handle_update_for_acl_config_item(self, config_item_list):
+        pass
+
+    async def handle_delete_for_acl_config_item(self, config_item_list):
+        pass
+
+    async def handle_create_for_acl_config_item(self, config_item_list):
+        pass

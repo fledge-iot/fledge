5dc973d95a93693836283925837db6ae70cf82c9
Deepanshu Yadav <rohantheyadav@gmail.com>
2022-07-25 12:48:07 +0530

    fixed error with import
    Signed-off-by: Deepanshu Yadav <rohantheyadav@gmail.com>

--- python/fledge/common/microservice_management_client/microservice_management_client.py
+++ python/fledge/common/microservice_management_client/microservice_management_client.py
@@ -17,7 +17,7 @@ __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
 __license__ = "Apache 2.0"
 __version__ = "${VERSION}"
 
-_logger = logger.setup(__name__, logging.DEBUG)
+_logger = logger.setup(__name__, level=logging.DEBUG)
 
 
 class MicroserviceManagementClient(object):

17f2d0e16b0531a25132766384bdc51addd95472
Mohit04tomar <M.singh19994@gmail.com>
2022-08-12 15:16:23 +0530

    Refactored Code
    Signed-off-by: Mohit04tomar <M.singh19994@gmail.com>

--- tests/system/memory_leak/test_memcheck.sh
+++ tests/system/memory_leak/test_memcheck.sh
@@ -9,13 +9,13 @@ export FLEDGE_ROOT=$(pwd)/fledge
 FLEDGE_TEST_BRANCH=$1     #here fledge_test_branch means branch of fledge repository that is needed to be scanned, default is devops
 
 cleanup(){
-  # sudo rm -rf /usr/local/fledge
+  # Removing tempo files, fledge and its plugin repository cloned by previous build of the Job 
+  echo "Removing Cloned repository and tmp files"
   rm -rf /tmp/*valgrind*.log /tmp/*valgrind*.xml
-  echo "Removig Repositories..."
   rm -rf fledge* reports && echo 'Done.'
 }
 
-#Setting up Fledge and installing its plugin
+# Setting up Fledge and installing its plugin
 setup(){
    ./scripts/setup fledge-south-sinusoid  ${FLEDGE_TEST_BRANCH} 
 }
@@ -83,12 +83,16 @@ setup_north_pi_egress () {
   echo 'North setup done'
 }
 
-generate_valgrind_logs(){
-  sleep ${TEST_RUN_TIME} 
+# This Function keep the fledge and its plugin running state for the "TEST_RUN_TIME" seconds then stop the fledge, So that data required for mem check be collected.
+collect_data(){
+  sleep ${TEST_RUN_TIME}
   cd ${FLEDGE_ROOT}/scripts/
   echo 'stopping fledge'
-  ./fledge stop 
+  ./fledge stop
   cd ../../
+}
+
+generate_valgrind_logs(){
   echo 'Creating reports directory';
   mkdir -p reports/test1 ; ls -lrth
   echo 'copying reports '
@@ -101,4 +105,6 @@ setup
 reset_fledge
 adding_sinusoid
 setup_north_pi_egress
+collect_data
 generate_valgrind_logs 
+

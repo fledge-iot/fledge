5b3aae4642430db0b05f94aaac21346b59a05827
Himanshu Vimal <himanshu.vimal@gmail.com>
2022-08-23 08:47:49 +0100

    FOGL-6784 Added Pip installation feature for developer
    Signed-off-by: Himanshu Vimal <himanshu.vimal@gmail.com>

--- python/fledge/services/core/api/python_packages.py
+++ python/fledge/services/core/api/python_packages.py
@@ -1,14 +1,20 @@
-import sys
 import logging
-
 import pkg_resources
-from aiohttp import web
+import sys
+import json
+import subprocess
+import asyncio
+import re
 
+from aiohttp import web
 from fledge.common.common import _FLEDGE_ROOT, _FLEDGE_DATA
 from fledge.common import logger
 from fledge.common.storage_client.payload_builder import PayloadBuilder
+from fledge.common.storage_client.storage_client import StorageClientAsync
+from fledge.common.storage_client.exceptions import StorageServerError
 from fledge.services.core import connect
 from fledge.common.audit_logger import AuditLogger
+from fledge.services.core import connect
 
 __author__ = "Himanshu Vimal"
 __copyright__ = "Copyright (c) 2019, Dianomic Systems Inc."
@@ -42,45 +48,91 @@ async def get_python_packages(request: web.Request) -> web.Response:
     installed_pkg_list = get_installed_pkg_list()
     return web.json_response({'packages': installed_pkg_list})
 
-'''
-async def install_package(request)
 
-    input_package_name = 'test'
-    input_package_version = ''
+async def install_package(request: web.Request) -> web.Response:
+    """
+    Args:
+        Request: '{ "package"   :   "numpy", 
+                    "version"   :   "1.2"   #optional
+                  }'
+
+    Returns:
+        Success: 
+
+    :Example:
+           curl -X POST http://localhost:8081/fledge/python/package -d '{"package":"numpy", "version":"1.23"}'
+    """
     
-    def check_installed_package(input_package):
-        if len(input_package.strip()) == 0:
-            return False
+    def get_installed_package_info(input_package):
 
-        package_list = [dist.project_name for dist in pkg_resources.working_set]
+        packages = pkg_resources.working_set
 
-        if input_package.lower() in package_list:  
-            _LOGGER.info("Input package already installed.")
-            return True
-        else:
-            _LOGGER.info("Input package not found.")
-            return False
+        for package in packages:
+            if package.project_name == input_package.lower():
+                return package.project_name, package.version
 
-    if len(input_package_version.strip()) == 0:
-        install_string = input_package_name
+        return None, None
+                    
+    # def is_canonical(version):
+    #     #regex in accordance with PEP-440
+    #     #Refer to https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
+    #     return re.match(r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$', input_package_version) is not None
+
+    data = await request.json()
+
+    input_package_name = data.get('package', None)
+    input_package_version = data.get('version', None)
+    #regex - PEP440 check for canonical versions
+    
+    if len(input_package_name.strip()) == 0:
+        return web.HTTPBadRequest(reason="Package name empty.")
+
+    #check version - semver
+
+    # if is_canonical(input_package_version) == None:
+    #     return web.HTTPBadRequest(reason="Package version not in canonical form.")
+    
+    if input_package_version == None:
+        install_cmd = input_package_name.lower()
     else:
-        install_string = input_package_name + '==' + input_package_version
+        install_cmd = input_package_name.lower() + "==" + input_package_version.lower()
+    
+    installed_package, installed_version = get_installed_package_info(input_package_name)
 
-    if check_installed_package(input_package_name):
-    #package installed, do audit log and return
-        _LOGGER.info("Package already installed")
+    if installed_package != None:
+         #Package already exists
+        _LOGGER.info("Package: {} Version: {} already installed.".format(installed_package, installed_version))
+        return web.HTTPConflict(reason="Package already installed.", 
+                                body=json.dumps({"message":"Package: {} Version:{} already installed"
+                                                .format(installed_package, installed_version)}))
     else:
-        #install package
         try:
-            code = subprocess.run([sys.executable, '-m', 'pip', 'install', install_string], capture_output=True, text=True, encoding='utf-8')
-            if code.returncode != 0:
-            #log error and output
-                _LOGGER.error(code.stdout)
-                _LOGGER.error(code.stderr)
-            #close code after output is processed
-            else:
-                _LOGGER.info("Package: {} successfully installed", format(input_package))
-        except subprocess.CalledProcessError as e:
-            _LOGGER.exception("Pip install exception: {}", format(str(e.output)))
+            # code = subprocess.run([sys.executable, '-m', 'pip', 'install', install_string], capture_output=True, text=True, encoding='utf-8')
+            pip_process = await asyncio.create_subprocess_exec('pip3 install', [install_cmd], stdout=asyncio.subprocess.PIPE, 
+                                                                stderr=asyncio.subprocess.PIPE)
+            
+            pip_stdout, pip_stderr = await pip_process.communicate()
+            if pip_process.returncode == 0:
+                _LOGGER.info("Package: {} successfully installed", format(input_package_name))
+                try:
+                    #Audit log entry: PIPIN
+                    storage_client = connect.get_storage_async()
+                    pip_audit_log = AuditLogger(storage_client)
+                    audit_message = {"message":"Package: {} Version:{} successfully installed"
+                                        .format(input_package_name, input_package_version)}
+                    await pip_audit_log.information('PIPIN', audit_message)
+                except:
+                    _LOGGER.exception("Failed to log the audit entry for PIPIN, while installing package {}", format(input_package_name))
 
-'''
+                return web.json_response("Package: {} successfully installed".format(input_package_name, input_package_version))
+            else:
+            #log error and output
+            #close code after output is processed
+                # _LOGGER.error(code.stdout)
+                _LOGGER.error(pip_stderr.decode().strip())
+                return web.HTTPNotFound(reason="Invalid package name")
+        except subprocess.CalledProcessError as ex:
+            _LOGGER.exception("Pip install exception: {}", format(str(ex.output)))
+            return web.HTTPError(reason=str(ex))
+        except Exception as ex:
+            return web.HTTPInternalServerError(reason=str(ex))
--- python/fledge/services/core/routes.py
+++ python/fledge/services/core/routes.py
@@ -34,7 +34,7 @@ from fledge.services.core.api import package_log
 from fledge.services.core.api.repos import configure as configure_repo
 from fledge.services.core.api.control_service import script_management
 from fledge.services.core.api.control_service import acl_management
-from fledge.services.core.api import pyhton_packages
+from fledge.services.core.api import python_packages
 
 __author__ = "Ashish Jabble, Praveen Garg, Massimiliano Pinto, Amarendra K Sinha"
 __copyright__ = "Copyright (c) 2017-2018 OSIsoft, LLC"
@@ -252,6 +252,7 @@ def setup(app):
     app.router.add_route('DELETE', '/fledge/service/{service_name}/ACL', acl_management.detach_acl_from_service)
 
     app.router.add_route('GET', '/fledge/python/packages', python_packages.get_python_packages)
+    app.router.add_route('POST', '/fledge/python/package', python_packages.install_package)
     # Proxy Admin API setup with regex
     proxy.admin_api_setup(app)
 

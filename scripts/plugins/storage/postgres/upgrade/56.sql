d9dd75ea46cf986bec7ed346c194de98427f8224
Praveen Garg <praveen.garg@nerdapplabs.com>
2022-08-25 15:30:10 +0530

    bump version for upgrade test

---
+++ scripts/plugins/storage/postgres/upgrade/56.sql
@@ -0,0 +1,6 @@
+-- Addition of new log codes
+
+INSERT INTO fledge.log_codes ( code, description )
+     VALUES
+        ( 'DASTD', 'X Asset deprecated' ),
+        ( 'YASTU', 'Y Asset un-deprecated' );

2786ae25b135febb3b53a5e47dd44eec34c707d3
Mark Riddoch <mark@dianomic.com>
2022-10-07 15:07:04 +0000

    Move from jq to Python to process JSON
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

---
+++ scripts/common/disk_usage.py
@@ -0,0 +1,15 @@
+"""
+Extract the disk usage  percentage and print it if the percentage
+is greater than or equal to a value passed in.
+The script takes as input the output of the GET /fledge/health/logging
+or GET /fledge/health/storage API call
+"""
+import json
+import sys
+
+if __name__ == '__main__':
+   json = json.loads(sys.stdin.readline())
+   item = json["disk"]
+   usage=item["usage"]
+   if usage >= int(sys.argv[1]):
+       print(usage)
---
+++ scripts/common/service_status.py
@@ -0,0 +1,14 @@
+"""
+Utility to extract the names of services in a particular state giventhe output of
+the API call GET /fledge/services
+"""
+
+import json
+import sys
+
+if __name__ == '__main__':
+   json = json.loads(sys.stdin.readline())
+   state = sys.argv[1]
+   for service in json["services"]:
+       if service["status"] == state:
+           print(service["name"])

ffd02ae3a92976952f36283f1079df685a89663b
Mark Riddoch <mark@dianomic.com>
2022-10-12 15:31:05 +0100

    FOGL-7001 Add basic healthcheck option (#845)
    * FOGL-7001 Add basic healthcheck option
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Allow for authentication
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Add current service health
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Correct output of unresponsive services
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Address review comments
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Move from jq to Python to process JSON
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Resolve review comments
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Address review comments
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Address review comments
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Address review comments
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

---
+++ scripts/common/audittime.py
@@ -0,0 +1,17 @@
+"""
+Extract the audit entries that have a timestamp that starts with
+the timestamp string passed in as an argument. The output is
+the details/name tag of the audit entry.
+
+The script takes as input the output of the GET /fledge/audit API call
+"""
+
+import json
+import sys
+
+if __name__ == '__main__':
+    json = json.loads(sys.stdin.readline())
+    ts = sys.argv[1]
+    for audit in json["audit"]:
+         if audit["timestamp"].startswith(ts):
+              print(audit["details"]["name"])

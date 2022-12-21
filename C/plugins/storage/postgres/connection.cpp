e21b095f493919a3ad15948ac4e76b55ab02ab2b
Mark Riddoch <mark@dianomic.com>
2022-12-21 12:12:15 +0000

    Fix issue raused in review comments
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/plugins/storage/postgres/connection.cpp
+++ C/plugins/storage/postgres/connection.cpp
@@ -1292,9 +1292,9 @@ SQLBuffer	sql;
 				const Value& modifier = (*iter)["modifier"];
 				for (Value::ConstValueIterator modifiers = modifier.Begin(); modifiers != modifier.End(); ++modifiers)
                 		{
-					if (iter->IsString())
+					if (modifiers->IsString())
 					{
-						string mod = iter->GetString();
+						string mod = modifiers->GetString();
 						if (mod.compare("allowzero") == 0)
 						{
 							allowZero = true;

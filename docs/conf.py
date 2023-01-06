9d8b023c5a429efd4dbb8a13e3861ae878d3b0e9
Mohit Tomar <m.singh19994@gmail.com>
2023-01-06 11:52:46 +0530

    DOCBRANCH set to develop
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -177,4 +177,4 @@ html_context = {
 
 # Pass Plugin DOCBRANCH argument in Makefile ; by default develop
 # NOTE: During release time we need to replace DOCBRANCH with actual released version
-subprocess.run(["make generated DOCBRANCH='2.1.0RC'"], shell=True, check=True)
+subprocess.run(["make generated DOCBRANCH='develop'"], shell=True, check=True)

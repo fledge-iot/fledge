a0ec27a31eb018da4df8b8abd1cb1652fe941930
Mark Riddoch <mark@dianomic.com>
2023-02-16 10:20:25 +0000

    FOGL-7419 Wakeup on demand poll when changing to interval poll mode
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/services/south/south.cpp
+++ C/services/south/south.cpp
@@ -875,6 +875,11 @@ void SouthService::processConfigChange(const string& categoryName, const string&
 				}
 				string units = m_configAdvanced.getValue("units");
 				string pollType = m_configAdvanced.getValue("pollType");
+				bool wakeup = false;
+				if (m_pollType == POLL_ON_DEMAND)
+				{
+					wakeup = true;
+				}
 				if (pollType.compare("Fixed Times") == 0)
 				{
 					m_pollType = POLL_FIXED;
@@ -889,6 +894,11 @@ void SouthService::processConfigChange(const string& categoryName, const string&
 
 					m_desiredRate.tv_sec  = 1;
 					m_desiredRate.tv_usec = 0;
+					if (wakeup)
+					{
+						// Wakup from on demand polling
+						m_pollCV.notify_all();
+					}
 				}
 				else if (pollType.compare("Interval") == 0
 						&& (newval != m_readingsPerSec || m_rateUnits.compare(units) != 0))
@@ -900,6 +910,21 @@ void SouthService::processConfigChange(const string& categoryName, const string&
 					calculateTimerRate();
 					m_currentRate = m_desiredRate;
 					m_timerfd = createTimerFd(m_desiredRate); // interval to be passed is in usecs
+					if (wakeup)
+					{
+						// Wakup from on demand polling
+						m_pollCV.notify_all();
+					}
+				}
+				else if (pollType.compare("Interval") == 0 && m_pollType != POLL_INTERVAL)
+				{
+					// Change to interval mode without the rate changing
+					m_pollType = POLL_INTERVAL;
+					if (wakeup)
+					{
+						// Wakup from on demand polling
+						m_pollCV.notify_all();
+					}
 				}
 				else if (pollType.compare("On Demand") == 0)
 				{
@@ -1436,7 +1461,7 @@ bool SouthService::syncToNextPoll()
 	time_t tim = time(0);
 	struct tm tm;
 	localtime_r(&tim, &tm);
-	unsigned long waitFor;
+	unsigned long waitFor = 0;
 
 	if (m_hours.size() == 0 && m_minutes.size() == 0 && m_seconds.size() == 0)
 	{
@@ -1593,8 +1618,9 @@ bool SouthService::syncToNextPoll()
 	uint64_t exp;
 	while (waitFor)
 	{
-		read(m_timerfd, &exp, sizeof(uint64_t));
-		waitFor--;
+		int s = read(m_timerfd, &exp, sizeof(uint64_t));
+		if (s > 0)
+			waitFor--;
 		if (m_shutdown)
 			return false;
 		if (m_pollType != POLL_FIXED)	// Configuration has change to the poll type

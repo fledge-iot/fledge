f897ec8c9d411c63dbb6062437a80b766e560cda
nandan <nandan.ghildiyal@gmail.com>
2023-03-31 11:07:35 +0530

    FOGL-7274: Replaced regex_replace function with JSONescape
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

--- C/common/storage_asset_tracking.cpp
+++ C/common/storage_asset_tracking.cpp
@@ -180,7 +180,7 @@ void StorageAssetTracker::updateCache(std::set<std::string> dpSet, StorageAssetT
                 unsigned int count = 0;
                 for (auto itr : dpSet)
                 {
-                        strDatapoints.append(std::regex_replace(itr, std::regex("\""), "\\\""));
+                        strDatapoints.append(JSONescape(itr));
                         strDatapoints.append(",");
                         count++;
                 }
@@ -210,7 +210,7 @@ void StorageAssetTracker::updateCache(std::set<std::string> dpSet, StorageAssetT
 		unsigned int count = 0;
 		for (auto itr : cacheRecord)
 		{
-			strDatapoints.append(std::regex_replace(itr, std::regex("\""), "\\\""));
+			strDatapoints.append(JSONescape(itr));
 			strDatapoints.append(",");
 			count++;
 		}
@@ -221,7 +221,7 @@ void StorageAssetTracker::updateCache(std::set<std::string> dpSet, StorageAssetT
                 {
                         if (cacheRecord.find(itr) == cacheRecord.end())
                         {
-				strDatapoints.append(std::regex_replace(itr, std::regex("\""), "\\\""));
+				strDatapoints.append(JSONescape(itr));
 	                        strDatapoints.append(",");
 				count++;
                         }

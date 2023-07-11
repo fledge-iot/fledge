24b4a6a50512bcf01139183d1e47478b37225b2f
Mohit Singh Tomar <M.singh19994@gmail.com>
2023-07-11 16:33:13 +0530

    Refacotred code, fixed typos and Done improvement in genrating xml report
    Signed-off-by: Mohit Singh Tomar <M.singh19994@gmail.com>

--- tests/system/memory_leak/test_memcheck.sh
+++ tests/system/memory_leak/test_memcheck.sh
@@ -7,7 +7,7 @@ source config.sh
 
 export FLEDGE_ROOT=$(pwd)/fledge
 
-FLEDGE_TEST_BRANCH=$1     #here fledge_test_branch means branch of fledge repository that is needed to be scanned, default is devops
+FLEDGE_TEST_BRANCH="$1"    # here Branch means branch of fledge repository that is needed to be scanned through valgrind, default is develop
 
 cleanup(){
   # Removing temporary files, fledge and its plugin repository cloned by previous build of the Job 
@@ -43,8 +43,8 @@ add_sinusoid(){
   echo
 }
 
-add_randomwalk(){
-  echo -e INFO: "Add South Randomwalk"
+add_random(){
+  echo -e INFO: "Add South Random"
   curl -sX POST "$FLEDGE_URL/service" -d \
   '{
      "name": "Random",
@@ -76,16 +76,16 @@ setup_north_pi_egress () {
            "value": "PI Web API"
         },
         "ServerHostname": {
-           "value": "'$2'"
+           "value": "'${PI_IP}'"
         },
         "ServerPort": {
            "value": "443"
         },
         "PIWebAPIUserId": {
-           "value": "'$3'"
+           "value": "'${PI_USER}'"
         },
         "PIWebAPIPassword": {
-           "value": "'$4'"
+           "value": "'${PI_PASSWORD}'"
         },
         "NamingScheme": {
            "value": "Backward compatibility"
@@ -116,15 +116,14 @@ generate_valgrind_logs(){
   echo 'Creating reports directory';
   mkdir -p reports/test1 ; ls -lrth
   echo 'copying reports '
-  cp -rf /tmp/*valgrind*.log reports/test1/. && echo 'copied'
-  rm -rf fledge*
+  cp -rf /tmp/*valgrind*.log /tmp/*valgrind*.xml reports/test1/. && echo 'copied'
 }
 
 cleanup
 setup
 reset_fledge
 add_sinusoid
-add_randomwalk
+add_random
 setup_north_pi_egress
 collect_data
 generate_valgrind_logs 

6b08e191f1d6e582292456109044472e93a3b09a
ashish-jabble <ashish@dianomic.com>
2023-08-11 17:42:57 +0530

    Downgrade fixes when records are there
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- scripts/plugins/storage/sqlitelb/downgrade/63.sql
+++ scripts/plugins/storage/sqlitelb/downgrade/63.sql
@@ -1,4 +1,4 @@
--- Drop control pipeline tables
-DROP TABLE IF EXISTS fledge.control_api;
-DROP TABLE IF EXISTS fledge.control_api_parameters;
+-- Drop control flow tables
 DROP TABLE IF EXISTS fledge.control_api_acl;
+DROP TABLE IF EXISTS fledge.control_api_parameters;
+DROP TABLE IF EXISTS fledge.control_api;

5c7a24796bdc3567b0518eef94b8eaeb32f1b11c
ashish-jabble <ashish@dianomic.com>
2023-08-16 12:01:43 +0530

    new audit log codes for ACL, Control Script and Pipeline in init.sql
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- scripts/plugins/storage/postgres/init.sql
+++ scripts/plugins/storage/postgres/init.sql
@@ -992,7 +992,11 @@ INSERT INTO fledge.log_codes ( code, description )
             ( 'USRAD', 'User Added' ),
             ( 'USRDL', 'User Deleted' ),
             ( 'USRCH', 'User Changed' ),
-            ( 'USRRS', 'User Restored' );
+            ( 'USRRS', 'User Restored' ),
+            ( 'ACLAD', 'ACL Added' ),( 'ACLCH', 'ACL Changed' ),( 'ACLDL', 'ACL Deleted' ),
+            ( 'CTSAD', 'Control Script Added' ),( 'CTSCH', 'Control Script Changed' ),('CTSDL', 'Control Script Deleted' ),
+            ( 'CTPAD', 'Control Pipeline Added' ),( 'CTPCH', 'Control Pipeline Changed' ),('CTPDL', 'Control Pipeline Deleted' )
+            ;
 
 --
 -- Configuration parameters

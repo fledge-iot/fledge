1633a64527cb58c3d00c92654c3599da74f6affe
Mark Riddoch <mark@dianomic.com>
2023-08-18 19:32:12 +0100

    FOGL-7959 Improve error handling for plugin load failure (#1142)
    * Error reporting improvements
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Improve JSON parse error reporting
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * FOGL-7959 Capture some of the common reasons plugins fail to load
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Fix typos
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Removing using namespace from string_utils header file
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Removing using namespace from string_utils header file
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Update test
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Update tests
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Fix unit tests
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Test checkin to add loggign to PR unit tester
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Using -j1 in RunAllTests
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Using -j1 in RunAllTests
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Include strign_utils in postgres test
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Use entire common library for Postgres test
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Add library location
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Add extra library requirements
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    ---------
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/common/include/string_utils.h
+++ C/common/include/string_utils.h
@@ -14,7 +14,6 @@
 #include <sstream>
 #include <iomanip>
 
-using namespace std;
 
 void StringReplace(std::string& StringToManage,
 		   const std::string& StringToSearch,
@@ -24,25 +23,28 @@ void StringReplaceAll(std::string& StringToManage,
 					  const std::string& StringToSearch,
 					  const std::string& StringReplacement);
 
-string StringSlashFix(const string& stringToFix);
+std::string StringSlashFix(const std::string& stringToFix);
 std::string evaluateParentPath(const std::string& path, char separator);
 std::string extractLastLevel(const std::string& path, char separator);
 
 void   StringStripCRLF(std::string& StringToManage);
-string StringStripWhiteSpacesAll(const std::string& original);
-string StringStripWhiteSpacesExtra(const  std::string& original);
+std::string StringStripWhiteSpacesAll(const std::string& original);
+std::string StringStripWhiteSpacesExtra(const  std::string& original);
 void StringStripQuotes(std::string& StringToManage);
 
-string urlEncode(const string& s);
-string urlDecode(const string& s);
-void StringEscapeQuotes(string& s);
+std::string urlEncode(const std::string& s);
+std::string urlDecode(const std::string& s);
+void StringEscapeQuotes(std::string& s);
 
 char *trim(char *str);
 std::string StringLTrim(const std::string& str);
 std::string StringRTrim(const std::string& str);
 std::string StringTrim(const std::string& str);
 
-bool IsRegex(const string &str);
+bool IsRegex(const std::string &str);
+
+std::string StringAround(const std::string& str, unsigned int pos,
+		unsigned int after = 30, unsigned int before = 10);
 
 
 #endif

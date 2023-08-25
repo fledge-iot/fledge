ccca75bc8bb779d0cc886f60b7a47fe4d430c2e5
Mark Riddoch <mark@dianomic.com>
2023-08-25 12:12:31 +0100

    FOGL-7349 Add persistence of SQLiteInMemory between executions (#1152)
    * FOGL-7349 Add peristance of SQLite in memory database
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Remove extra debug
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Fix unit test cmakefiles to access new sqlite_common include file
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    * Update following review comments
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>
    
    ---------
    
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/plugins/storage/sqlite/common/include/connection.h
+++ C/plugins/storage/sqlite/common/include/connection.h
@@ -23,28 +23,11 @@
 
 #define TRACK_CONNECTION_USER		0 // Set to 1 to get dianositcs about connection pool use
 
-#define _DB_NAME                  "/fledge.db"
-#define READINGS_DB_NAME_BASE     "readings"
 #define READINGS_DB_FILE_NAME     "/" READINGS_DB_NAME_BASE "_1.db"
 #define READINGS_DB               READINGS_DB_NAME_BASE "_1"
 #define READINGS_TABLE            "readings"
 #define READINGS_TABLE_MEM       READINGS_TABLE "_1"
 
-#define LEN_BUFFER_DATE 100
-#define F_TIMEH24_S             "%H:%M:%S"
-#define F_DATEH24_S             "%Y-%m-%d %H:%M:%S"
-#define F_DATEH24_M             "%Y-%m-%d %H:%M"
-#define F_DATEH24_H             "%Y-%m-%d %H"
-// This is the default datetime format in Fledge: 2018-05-03 18:15:00.622
-#define F_DATEH24_MS            "%Y-%m-%d %H:%M:%f"
-// Format up to seconds
-#define F_DATEH24_SEC           "%Y-%m-%d %H:%M:%S"
-#define SQLITE3_NOW             "strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')"
-// The default precision is milliseconds, it adds microseconds and timezone
-#define SQLITE3_NOW_READING     "strftime('%Y-%m-%d %H:%M:%f000+00:00', 'now')"
-#define SQLITE3_FLEDGE_DATETIME_TYPE "DATETIME"
-
-#define  DB_CONFIGURATION "PRAGMA busy_timeout = 5000; PRAGMA cache_size = -4000; PRAGMA journal_mode = WAL; PRAGMA secure_delete = off; PRAGMA journal_size_limit = 4096000;"
 
 // Set plugin name for log messages
 #ifndef PLUGIN_LOG_NAME

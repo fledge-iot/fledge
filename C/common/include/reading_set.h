cd1d374d5d7653af666bfb650b072a6c401cd2da
Mark Riddoch <mark@dianomic.com>
2023-09-19 15:53:01 +0000

    Experiment with removing virtual from reading set destructor
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/common/include/reading_set.h
+++ C/common/include/reading_set.h
@@ -28,7 +28,7 @@ class ReadingSet {
 		ReadingSet();
 		ReadingSet(const std::string& json);
 		ReadingSet(const std::vector<Reading *>* readings);
-		virtual ~ReadingSet();
+		~ReadingSet();			// This should be virtual
 
 		unsigned long			getCount() const { return m_count; };
 		const Reading			*operator[] (const unsigned int idx) {

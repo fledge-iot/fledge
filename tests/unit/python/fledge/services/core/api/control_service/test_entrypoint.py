a0d9763ff8b3b5b0e54e32bb407636292773c189
ashish-jabble <ashish@dianomic.com>
2023-09-29 11:40:18 +0530

    internal def unit tests added in Control Flow API
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- tests/unit/python/fledge/services/core/api/control_service/test_entrypoint.py
+++ tests/unit/python/fledge/services/core/api/control_service/test_entrypoint.py
@@ -26,7 +26,7 @@ async def mock_coro(*args):
 @pytest.allure.feature("unit")
 @pytest.allure.story("api", "entrypoint")
 class TestEntrypoint:
-    """ Control Flow Entrypoint API tests"""
+    """ Control Flow Entrypoint API tests """
 
     @pytest.fixture
     def client(self, loop, test_client):
@@ -87,13 +87,15 @@ class TestEntrypoint:
         else:
             rv1 = asyncio.ensure_future(mock_coro(storage_result))
             rv2 = asyncio.ensure_future(mock_coro(True))
-        with patch.object(entrypoint, '_get_entrypoint', return_value=rv1):
-            with patch.object(entrypoint, '_get_permitted', return_value=rv2):
+        with patch.object(entrypoint, '_get_entrypoint', return_value=rv1) as patch_entrypoint:
+            with patch.object(entrypoint, '_get_permitted', return_value=rv2) as patch_permitted:
                 resp = await client.get('/fledge/control/manage/{}'.format(ep_name))
                 assert 200 == resp.status
                 json_response = json.loads(await resp.text())
                 assert 'permitted' in json_response
                 assert storage_result == json_response
+            assert 1 == patch_permitted.call_count
+        patch_entrypoint.assert_called_once_with(ep_name)
 
     async def test_create_entrypoint_in_use(self, client):
         ep_name = "SetLatheSpeed"
@@ -139,8 +141,6 @@ class TestEntrypoint:
                     assert {'name': ep_name, 'parameter': 'rpm', 'value': '100', 'constant': 'f'
                             } == json.loads(insert_payload)
             elif table == 'control_api_acl':
-                # allow is empty in given payload
-                # TODO: in future
                 pass
             return insert_result
 
@@ -152,7 +152,8 @@ class TestEntrypoint:
             arv = asyncio.ensure_future(mock_coro(None))
         with patch.object(connect, 'get_storage_async', return_value=storage_client_mock):
             with patch.object(storage_client_mock, 'query_tbl', return_value=rv) as patch_query_tbl:
-                with patch.object(storage_client_mock, 'insert_into_tbl', side_effect=i_result):
+                with patch.object(storage_client_mock, 'insert_into_tbl', side_effect=i_result
+                                  ) as patch_insert_tbl:
                     with patch.object(AuditLogger, '__init__', return_value=None):
                         with patch.object(AuditLogger, 'information', return_value=arv) as audit_info_patch:
                             resp = await client.post('/fledge/control/manage', data=json.dumps(payload))
@@ -162,6 +163,7 @@ class TestEntrypoint:
                             assert {'message': '{} control entrypoint has been created successfully.'.format(ep_name)
                                     } == json_response
                         audit_info_patch.assert_called_once_with('CTEAD', payload)
+                assert 3 == patch_insert_tbl.call_count
             patch_query_tbl.assert_called_once_with('control_api')
 
     async def test_update_entrypoint_not_found(self, client):
@@ -370,3 +372,179 @@ class TestEntrypoint:
                                                            svc_info[1], dispatch_payload)
             patch_service.assert_called_once_with()
 
+    @pytest.mark.parametrize("identifier, identifier_value", [
+        (0, 'write'),
+        (1, 'operation'),
+        ('write', 0),
+        ('operation', 1)
+    ])
+    async def test__get_type(self, identifier, identifier_value):
+        assert identifier_value == await entrypoint._get_type(identifier)
+
+    @pytest.mark.parametrize("identifier, identifier_value", [
+        (0, 'broadcast'),
+        (1, 'service'),
+        (2, 'asset'),
+        (3, 'script'),
+        ('broadcast', 0),
+        ('service', 1),
+        ('asset', 2),
+        ('script', 3)
+    ])
+    async def test__get_destination(self, identifier, identifier_value):
+        assert identifier_value == await entrypoint._get_destination(identifier)
+
+    async def test__update_params(self):
+        ep_name = "SetLatheSpeed"
+        old = {'x': '640', 'y': '480'}
+        new = {'x': '180', 'z': '90'}
+        is_constant = 't'
+        storage_client_mock = MagicMock(StorageClientAsync)
+        rows_affected = {"response": "updated", "rows_affected": 1}
+        rv = await mock_coro(rows_affected) if sys.version_info >= (3, 8) else (
+            asyncio.ensure_future(mock_coro(rows_affected)))
+        tbl_name = 'control_api_parameters'
+        delete_payload = {"where": {"column": "name", "condition": "=", "value": ep_name,
+                                    "and": {"column": "constant", "condition": "=", "value": "t",
+                                            "and": {"column": "parameter", "condition": "=", "value": list(old)[1]}}}}
+        insert_payload = {'name': ep_name, 'parameter': 'z', 'value': new['z'], 'constant': 't'}
+        update_payload = {"where": {"column": "name", "condition": "=", "value": ep_name,
+                                    "and": {"column": "constant", "condition": "=", "value": "t",
+                                            "and": {"column": "parameter", "condition": "=", "value": "x"}}},
+                          "values": {"value": new['x']}}
+        with patch.object(storage_client_mock, 'update_tbl', return_value=rv) as patch_update_tbl:
+            with patch.object(storage_client_mock, 'delete_from_tbl', return_value=rv) as patch_delete_tbl:
+                with patch.object(storage_client_mock, 'insert_into_tbl', return_value=rv) as patch_insert_tbl:
+                    await entrypoint._update_params(ep_name, old, new, is_constant, storage_client_mock)
+                args, _ = patch_insert_tbl.call_args
+                assert tbl_name == args[0]
+                assert insert_payload == json.loads(args[1])
+            args, _ = patch_delete_tbl.call_args
+            assert tbl_name == args[0]
+            assert delete_payload == json.loads(args[1])
+        args, _ = patch_update_tbl.call_args
+        assert tbl_name == args[0]
+        assert update_payload == json.loads(args[1])
+
+    async def test__get_entrypoint(self):
+        ep_name = "SetLatheSpeed"
+        storage_client_mock = MagicMock(StorageClientAsync)
+        payload = {"where": {"column": "name", "condition": "=", "value": ep_name}}
+        storage_result1 = {"count": 1, "rows": [
+            {'name': ep_name, 'description': 'Perform lathe Speed', 'type': 'operation', 'operation_name': 'Speed',
+             'destination': 'broadcast', 'destination_arg': '', 'anonymous': True,
+             'constants': {}, 'variables': {},
+             'allow': []}]}
+        storage_result2 = {"count": 0, "rows": []}
+        if sys.version_info >= (3, 8):
+            rv1 = await mock_coro(storage_result1)
+            rv2 = await mock_coro(storage_result2)
+            rv3 = await mock_coro(storage_result2)
+        else:
+            rv1 = asyncio.ensure_future(mock_coro(storage_result1))
+            rv2 = asyncio.ensure_future(mock_coro(storage_result2))
+            rv3 = asyncio.ensure_future(mock_coro(storage_result2))
+        with patch.object(connect, 'get_storage_async', return_value=storage_client_mock):
+            with patch.object(storage_client_mock, 'query_tbl_with_payload', side_effect=[rv1, rv2, rv3]
+                              ) as patch_query_tbl:
+                await entrypoint._get_entrypoint(ep_name)
+            assert 3 == patch_query_tbl.call_count
+            args1 = patch_query_tbl.call_args_list[0]
+            assert 'control_api' == args1[0][0]
+            assert payload == json.loads(args1[0][1])
+            args2 = patch_query_tbl.call_args_list[1]
+            assert 'control_api_parameters' == args2[0][0]
+            assert payload == json.loads(args2[0][1])
+            args3 = patch_query_tbl.call_args_list[2]
+            assert 'control_api_acl' == args3[0][0]
+            assert payload == json.loads(args3[0][1])
+
+    @pytest.mark.parametrize("payload", [
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'operation',
+         'operation_name': 'OP', 'destination': 'script', 'script': 'S1', 'anonymous': False},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'write',
+         'destination': 'script', 'script': 'S1', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'operation',
+         'operation_name': 'OP', 'destination': 'asset', 'asset': 'AS'},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'write',
+         'destination': 'asset', 'asset': 'AS', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'operation',
+         'operation_name': 'OP', 'destination': 'broadcast'},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'write',
+         'destination': 'broadcast', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}, 'anonymous': True},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'operation',
+         'operation_name': 'OP', 'destination': 'service', 'service': 'Camera'},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'write',
+         'destination': 'service', 'service': 'Camera', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'operation',
+         'operation_name': 'OP', 'destination': 'script', 'script': 'S1', 'anonymous': False,
+         'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/16'}}
+    ])
+    async def test__check_parameters(self, payload):
+        cols = await entrypoint._check_parameters(payload)
+        assert isinstance(cols, dict)
+
+    @pytest.mark.parametrize("payload", [
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'operation',
+         'operation_name': 'OP', 'destination': 'script', 'script': 'S1', 'anonymous': False},
+        {'name': 'FocusCamera', 'description': 'Perform focus on camera', 'type': 'write',
+         'destination': 'script', 'script': 'S1', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}},
+        {'anonymous': True},
+        {'description': 'updated'},
+        {'type': 'operation', 'operation_name': 'Distance'},
+        {'type': 'operation', 'operation_name': 'Test', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}},
+        {'type': 'write', 'constants': {'unit': 'cm'}, 'variables': {'aperture': 'f/11'}},
+        {'destination': 'asset', 'asset': 'AS'},
+        {'constants': {'unit': 'cm'}},
+        {'variables': {'aperture': 'f/11'}}
+    ])
+    async def test__check_parameters_without_required_keys(self, payload):
+        cols = await entrypoint._check_parameters(payload, skip_required=True)
+        assert isinstance(cols, dict)
+
+    @pytest.mark.parametrize("payload, exception_name, error_msg", [
+        # ({"a": 1}, KeyError,
+        #  "{'name', 'type', 'destination', 'description'} required keys are missing in request payload.")
+        ({"name": 1}, ValueError, "Control entrypoint name should be in string."),
+        ({"name": ""}, ValueError, "Control entrypoint name cannot be empty."),
+        ({"description": 1}, ValueError, "Control entrypoint description should be in string."),
+        ({"description": ""}, ValueError, "Control entrypoint description cannot be empty."),
+        ({"type": 1}, ValueError, "Control entrypoint type should be in string."),
+        ({"type": ""}, ValueError, "Control entrypoint type cannot be empty."),
+        ({"type": "Blah"}, ValueError, "Possible types are: ['write', 'operation']."),
+        ({"type": "operation"}, KeyError, "operation_name KV pair is missing."),
+        ({"type": "operation", "operation_name": ""}, ValueError, "Control entrypoint operation name cannot be empty."),
+        ({"type": "operation", "operation_name": 1}, ValueError,
+         "Control entrypoint operation name should be in string."),
+        ({"destination": ""}, ValueError, "Control entrypoint destination cannot be empty."),
+        ({"destination": 1}, ValueError, "Control entrypoint destination should be in string."),
+        ({"destination": "Blah"}, ValueError,
+         "Possible destination values are: ['broadcast', 'service', 'asset', 'script']."),
+        ({"destination": "script", "destination_arg": ""}, KeyError, "script destination argument is missing."),
+        ({"destination": "script", "script": 1}, ValueError,
+         "Control entrypoint destination argument should be in string."),
+        ({"destination": "script", "script": ""}, ValueError,
+         "Control entrypoint destination argument cannot be empty."),
+        ({"anonymous": "t"}, ValueError, "anonymous should be a bool."),
+        ({"constants": "t"}, ValueError, "constants should be a dictionary."),
+        ({"type": "write", "constants": {}}, ValueError, "constants should not be empty."),
+        ({"type": "write", "constants": None}, ValueError,
+         "For type write constants must have passed in payload and cannot have empty value."),
+        ({"variables": "t"}, ValueError, "variables should be a dictionary."),
+        ({"type": "write", "constants": {"unit": "cm"}, "variables": {}}, ValueError, "variables should not be empty."),
+        ({"type": "write", "constants": {"unit": "cm"}, "variables": None}, ValueError,
+         "For type write variables must have passed in payload and cannot have empty value."),
+        ({"allow": "user"}, ValueError, "allow should be an array of list of users.")
+    ])
+    async def test_bad__check_parameters(self, payload, exception_name, error_msg):
+        with pytest.raises(Exception) as exc_info:
+            await entrypoint._check_parameters(payload, skip_required=True)
+        assert exc_info.type is exception_name
+        assert exc_info.value.args[0] == error_msg
+
+    # TODO: add more tests
+    """
+        a) authentication based
+        b) allow
+        c) exception handling tests
+    """

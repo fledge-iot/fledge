d914e41a65e9f964c4ff654a70eae86f2281f18c
ashish-jabble <ashish@dianomic.com>
2024-01-16 15:16:38 +0530

    upgrade and downgrade scripts added for PostgreSQL engine
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- scripts/plugins/storage/postgres/init.sql
+++ scripts/plugins/storage/postgres/init.sql
@@ -699,6 +699,7 @@ CREATE INDEX fki_user_asset_permissions_fk2
 CREATE TABLE fledge.scheduled_processes (
              name   character varying(255)  NOT NULL, -- Name of the process
              script jsonb,                            -- Full path of the process
+             priority INTEGER NOT NULL DEFAULT 999,   -- priority to run for STARTUP
   CONSTRAINT scheduled_processes_pkey PRIMARY KEY ( name ) );
 
 
@@ -1063,13 +1064,13 @@ INSERT INTO fledge.scheduled_processes (name, script) VALUES ('restore', '["task
 
 -- South, Notification, North Tasks
 --
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'south_c',           '["services/south_c"]'          );
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'notification_c',    '["services/notification_c"]'   );
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'north_c',           '["tasks/north_c"]'             );
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'north',             '["tasks/north"]'               );
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'north_C',           '["services/north_C"]'          );
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'dispatcher_c',      '["services/dispatcher_c"]'     );
-INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'bucket_storage_c',  '["services/bucket_storage_c"]' );
+INSERT INTO fledge.scheduled_processes (name, script, priority)   VALUES ( 'south_c',           '["services/south_c"]',          100 );
+INSERT INTO fledge.scheduled_processes (name, script, priority)   VALUES ( 'notification_c',    '["services/notification_c"]',   30  );
+INSERT INTO fledge.scheduled_processes (name, script)             VALUES ( 'north_c',           '["tasks/north_c"]'                  );
+INSERT INTO fledge.scheduled_processes (name, script)             VALUES ( 'north',             '["tasks/north"]'                    );
+INSERT INTO fledge.scheduled_processes (name, script, priority)   VALUES ( 'north_C',           '["services/north_C"]',          200 );
+INSERT INTO fledge.scheduled_processes (name, script, priority)   VALUES ( 'dispatcher_c',      '["services/dispatcher_c"]',     20  );
+INSERT INTO fledge.scheduled_processes (name, script, priority)   VALUES ( 'bucket_storage_c',  '["services/bucket_storage_c"]', 10  );
 
 -- Automation script tasks
 --

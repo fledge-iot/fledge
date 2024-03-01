0fa2f208259ee1a200d0bdb31b69773cb12832f7
gnandan <111729765+gnandan@users.noreply.github.com>
2024-03-01 11:48:11 +0530

    FOGL-8442 : periodic task for update alerts has been added (#1283)
    * FOGL-8441 : Method to getalert and raise alert added to management client
    
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

---
+++ C/tasks/check_updates/check_updates.cpp
@@ -0,0 +1,189 @@
+/*
+ * Fledge Check Updates
+ *
+ * Copyright (c) 2024 Dianomic Systems
+ *
+ * Released under the Apache 2.0 Licence
+ *
+ * Author: Devki Nandan Ghildiyal
+ */
+
+#include <check_updates.h>
+#include <logger.h>
+
+#include <cstdlib>
+#include <thread>
+#include <csignal>
+#include <fstream>
+#include <errno.h>
+#include <cstring>
+
+using namespace std;
+
+volatile std::sig_atomic_t signalReceived = 0;
+
+static void signalHandler(int signal)
+{
+	signalReceived = signal;
+}
+
+
+/**
+ * Constructor for CheckUpdates
+ */
+CheckUpdates::CheckUpdates(int argc, char** argv) : FledgeProcess(argc, argv)
+{
+	std::string paramName;
+	paramName = getName();
+	m_logger = new Logger(paramName);
+	m_logger->info("CheckUpdates starting - parameters name :%s:", paramName.c_str() );
+	m_mgtClient = this->getManagementClient();
+
+}
+
+/**
+ * Destructor for CheckUpdates
+ */
+CheckUpdates::~CheckUpdates()
+{
+}
+
+/**
+ * CheckUpdates run method, called by the base class to start the process and do the actual work.
+ */
+void CheckUpdates::run()
+{
+	// We handle these signals, add more if needed
+	std::signal(SIGINT,  signalHandler);
+	std::signal(SIGSTOP, signalHandler);
+	std::signal(SIGTERM, signalHandler);
+
+
+	if (!m_dryRun)
+	{
+		raiseAlerts();
+	}
+	processEnd();
+}
+
+/**
+ * Execute the raiseAlerts, create an alert for all the packages for which update is available
+ */
+void CheckUpdates::raiseAlerts()
+{
+	m_logger->debug("raiseAlerts running");
+	try
+	{
+		for (auto key: getUpgradablePackageList())
+		{
+			std::string message = "A newer version of " + key + " is available for upgrade";
+			std::string urgency = "normal";
+			if (!m_mgtClient->raiseAlert(key,message,urgency))
+			{
+				m_logger->error("Failed to raise an alert for key=%s,message=%s,urgency=%s", key.c_str(), message.c_str(), urgency.c_str());
+			}
+		}
+
+	}
+	catch (...)
+	{
+		try
+		{
+			std::exception_ptr p = std::current_exception();
+			std::rethrow_exception(p);
+		}
+		catch(const std::exception& e)
+		{
+			m_logger->error("Failed to raise alert : %s", e.what());
+		}
+
+	}
+}
+
+/**
+ * Logs process end message
+ */
+
+void CheckUpdates::processEnd()
+{
+	m_logger->debug("raiseAlerts completed");
+}
+
+/**
+ * Fetch package manager name 
+ */
+
+std::string CheckUpdates::getPackageManager() 
+{
+	std::string command = "command -v yum || command -v apt-get";
+	std::string result = "";
+	char buffer[128];
+	
+	// Open pipe to file
+	FILE* pipe = popen(command.c_str(), "r");
+	if (!pipe)
+	{
+		m_logger->error("getPackageManager: popen call failed : %s",strerror(errno));
+		return "";
+	}
+	// read till end of process:
+	while (!feof(pipe))
+	{
+		if (fgets(buffer, 128, pipe) != NULL)
+			result += buffer;
+	}
+
+	pclose(pipe);
+
+	if (result.find("apt") != std::string::npos)
+		return "apt";
+	if (result.find("yum") != std::string::npos)
+		return "yum";
+
+	m_logger->warn("Unspported environment %s", result.c_str() );
+	return "";
+}
+
+/**
+ * Fetch a list of all the package name for which upgrade is available
+ */
+std::vector<std::string> CheckUpdates::getUpgradablePackageList() 
+{
+	std::string packageManager = getPackageManager();
+	std::vector<std::string> packageList;
+	if(!packageManager.empty())
+	{
+		std::string command = "(sudo apt update && sudo apt list --upgradeable) 2>/dev/null | grep '^fledge' | cut -d'/' -f1 ";
+		if (packageManager.find("yum") != std::string::npos)
+		{
+			command = "(sudo yum check-update && sudo yum list updates) 2>/dev/null | grep '^fledge' | cut -d' ' -f1 ";
+		}	
+
+		FILE* pipe = popen(command.c_str(), "r");
+		if (!pipe)
+		{
+			m_logger->error("getUpgradablePackageList: popen call failed : %s",strerror(errno));
+			return packageList;
+		}
+
+		char buffer[128];
+		while (!feof(pipe))
+		{
+			if (fgets(buffer, 128, pipe) != NULL)
+			{
+				//strip out newline characher
+				int len = strlen(buffer) - 1;
+				if (*buffer && buffer[len] == '\n')
+					buffer[len] = '\0';
+
+				packageList.emplace_back(buffer);
+
+			}
+		}
+		
+		pclose(pipe);
+	}
+
+	return packageList;
+}
+
--- scripts/plugins/storage/sqlitelb/init.sql
+++ scripts/plugins/storage/sqlitelb/init.sql
@@ -821,6 +821,7 @@ INSERT INTO fledge.scheduled_processes (name, script)   VALUES ( 'purge_system',
 INSERT INTO fledge.scheduled_processes ( name, script ) VALUES ( 'stats collector',     '["tasks/statistics"]'  );
 INSERT INTO fledge.scheduled_processes ( name, script ) VALUES ( 'FledgeUpdater',       '["tasks/update"]'      );
 INSERT INTO fledge.scheduled_processes ( name, script ) VALUES ( 'certificate checker', '["tasks/check_certs"]' );
+INSERT INTO fledge.scheduled_processes ( name, script ) VALUES ( 'update checker',      '["tasks/check_updates"]');
 
 -- Storage Tasks
 --
@@ -891,6 +892,19 @@ INSERT INTO fledge.schedules ( id, schedule_name, process_name, schedule_type,
                 3,                                      -- schedule_type (interval)
                 NULL,                                   -- schedule_time
                 '00:00:15',                             -- schedule_interval
+                't',                                   -- exclusive
+                't'                                    -- enabled
+              );
+
+-- Update checker
+INSERT INTO fledge.schedules ( id, schedule_name, process_name, schedule_type,
+                                schedule_time, schedule_interval, exclusive, enabled )
+       VALUES ( '852cd8e4-3c29-440b-89ca-2c7691b0450d', -- id
+                'update checker',                       -- schedule_name
+                'update checker',                       -- process_name
+                2,                                      -- schedule_type (timed)
+                '00:05:00',                             -- schedule_time
+                '00:00:00',                             -- schedule_interval
                 't',                                   -- exclusive
                 't'                                    -- enabled
               );

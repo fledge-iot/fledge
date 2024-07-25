80fcfca0ee6b5ef7d5071e7f14d53c718ec592c8
Himanshu Vimal <67678828+cyberwalk3r@users.noreply.github.com>
2024-07-25 14:01:06 +0530

    FOGL-8913: Fix readings count getting doubled on restart. (#1430)
    - Reverting changes done in FOGL-8285.
    
    Signed-off-by: Himanshu Vimal <himanshu.vimal@gmail.com>

--- C/services/north/data_load.cpp
+++ C/services/north/data_load.cpp
@@ -105,18 +105,12 @@ void DataLoad::restart()
  */
 bool DataLoad::setDataSource(const string& source)
 {
-	if (source.compare("statistics") == 0) {
+	if (source.compare("statistics") == 0)
 		m_dataSource = SourceStatistics;
-		m_lastFetched = 0;	// Reset on source change
-	}
-	else if (source.compare("readings") == 0) {
+	else if (source.compare("readings") == 0)
 		m_dataSource = SourceReadings;
-		m_lastFetched = 0;	// Reset on source change
-	}
-	else if (source.compare("audit") == 0) {
+	else if (source.compare("audit") == 0)
 		m_dataSource = SourceAudit;
-		m_lastFetched = 0;	// Reset on source change
-	}
 	else
 	{
 		Logger::getLogger()->error("Unsupported source '%s' for north service '%s'",

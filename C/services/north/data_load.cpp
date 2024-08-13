089fc72225e542f60b6b8a645398721283ac9e77
Mark Riddoch <mark@dianomic.com>
2024-08-13 14:16:58 +0000

    Resolve all cases and fix uninitialised variable
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- C/services/north/data_load.cpp
+++ C/services/north/data_load.cpp
@@ -417,7 +417,7 @@ void DataLoad::bufferReadings(ReadingSet *readings)
 ReadingSet *DataLoad::fetchReadings(bool wait)
 {
 	unique_lock<mutex> lck(m_qMutex);
-	while (m_queue.empty())
+	while (m_shutdown == false && m_queue.empty())
 	{
 		if (m_perfMonitor && m_perfMonitor->isCollecting())
 		{
@@ -433,9 +433,13 @@ ReadingSet *DataLoad::fetchReadings(bool wait)
 			return NULL;
 		}
 	}
-	ReadingSet *rval = m_queue.front();
-	m_queue.pop_front();
-	if (m_queue.size() < m_prefetchLimit)	// Read another block if we have less than 5 already queued
+	ReadingSet *rval = NULL;
+	if (!m_queue.empty())
+	{
+		rval = m_queue.front();
+		m_queue.pop_front();
+	}
+	if (m_queue.size() < m_prefetchLimit && m_shutdown == false)	// Read another block if we have less than 5 already queued
 	{
 		triggerRead(m_blockSize);
 	}

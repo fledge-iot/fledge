bf3906458c625eec515f3dd7bca1a21294ac649d
ashish-jabble <ashish@dianomic.com>
2025-04-24 11:29:20 +0530

    Restricted admin when we add user with systemctl role
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- python/fledge/services/core/api/auth.py
+++ python/fledge/services/core/api/auth.py
@@ -425,6 +425,9 @@ async def create_user(request):
     if not (await is_valid_role(role_id)):
         msg = "Invalid role ID."
         raise web.HTTPBadRequest(reason=msg, body=json.dumps({"message": msg}))
+    if role_id == 6:
+        msg = "We cannot create a user with systemctl role."
+        raise web.HTTPBadRequest(reason=msg, body=json.dumps({"message": msg}))
     users = await User.Objects.all()
     unames = [u['uname'] for u in users]
     if username in unames:

b450fca4264632a20564bd1b5b5ac7989b3aad33
Ray Verhoeff <ray@dianomic.com>
2025-11-21 16:53:05 -0500

    Add support for Windows library loading
    Signed-off-by: Ray Verhoeff <ray@dianomic.com>

--- C/services/common/include/binary_plugin_handle.h
+++ C/services/common/include/binary_plugin_handle.h
@@ -1,61 +1,112 @@
-
 #ifndef _BINARY_PLUGIN_HANDLE_H
 #define _BINARY_PLUGIN_HANDLE_H
 /*
- * Fledge plugin handle related
+ * Fledge Binary Plugin
  *
- * Copyright (c) 2018 OSisoft, LLC
+ * Copyright (c) 2018-2025 Dianomic Systems
  *
  * Released under the Apache 2.0 Licence
  *
  * Author: Amandeep Singh Arora
  */
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
 #include <logger.h>
 #include <dlfcn.h>
 #include <plugin_handle.h>
 #include <plugin_manager.h>
 
+#ifdef __linux__
+#define SharedLibOpen(path, flags) (PLUGIN_HANDLE) dlopen(path, flags)
+#define SharedLibClose(handle) dlclose(handle)
+#define SharedLibProcAddress(handle, sym) (void *)dlsym(handle, sym)
+#define SharedLibErrorMsg dlerror
+#define LoadLazy	RTLD_LAZY
+#define LoadLazyGlobal RTLD_LAZY | RTLD_GLOBAL
+#elif _WIN64
+static char sharedLibMessage[128];
+static char *getWindowsErrorMessage()
+{
+	DWORD errorCode = GetLastError();
+	if (errorCode == 0)
+	{
+		sharedLibMessage[0] = '\0';
+	}
+	else
+	{
+		if (0 == FormatMessage(
+			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			errorCode,
+			0,
+			sharedLibMessage,
+			sizeof(sharedLibMessage), NULL))
+			{
+				sharedLibMessage[0] = '\0';
+			}
+	}
+
+	return sLibMessage;
+}
+
+#define SharedLibOpen(path, flags) (PLUGIN_HANDLE)LoadLibrary(path)
+#define SharedLibClose(handle) FreeLibrary((HMODULE)handle)
+#define SharedLibProcAddress(handle, sym) (void *)GetProcAddress(handle, sym)
+#define SharedLibErrorMsg getWindowsErrorMessage
+#define LoadLazy 0
+#define LoadLazyGlobal 0
+#endif
+
 /**
- * The BinaryPluginHandle class is used to represent an interface to 
+ * The BinaryPluginHandle class is used to represent an interface to
  * a plugin that is available in a binary format
  */
 class BinaryPluginHandle : public PluginHandle
 {
-	public:
-		// for the Storage plugin
-		BinaryPluginHandle(const char *name, const char *path, tPluginType type) {
-			dlerror();	// Clear the existing error
-			handle = dlopen(path, RTLD_LAZY);
-			if (!handle)
-			{
-				Logger::getLogger()->error("Unable to load storage plugin %s, %s",
-						name, dlerror());
-			}
-
-			Logger::getLogger()->debug("%s - storage plugin / RTLD_LAZY - name :%s: path :%s:", __FUNCTION__, name, path);
+public:
+	// for the Storage plugin
+	BinaryPluginHandle(const char *name, const char *path, tPluginType type)
+	{
+		SharedLibErrorMsg(); // Clear the existing error
+		handle = SharedLibOpen(path, LoadLazy);
+		if (!handle)
+		{
+			Logger::getLogger()->error("Unable to load storage plugin %s, %s",
+									   name, SharedLibErrorMsg());
 		}
 
-		// for all the others plugins
-		BinaryPluginHandle(const char *name, const char *path)                   {
-			dlerror();	// Clear the existing error
-			handle = dlopen(path, RTLD_LAZY|RTLD_GLOBAL);
-			if (!handle)
-			{
-				Logger::getLogger()->error("Unable to load plugin %s, %s",
-						name, dlerror());
-			}
+		Logger::getLogger()->debug("%s - storage plugin / RTLD_LAZY - name :%s: path :%s:", __FUNCTION__, name, path);
+	}
 
-			Logger::getLogger()->debug("%s - other plugin / RTLD_LAZY|RTLD_GLOBAL - name :%s: path :%s:", __FUNCTION__, name, path);
+	// for all the others plugins
+	BinaryPluginHandle(const char *name, const char *path)
+	{
+		SharedLibErrorMsg(); // Clear the existing error
+		handle = SharedLibOpen(path, LoadLazyGlobal);
+		if (!handle)
+		{
+			Logger::getLogger()->error("Unable to load plugin %s, %s",
+									   name, SharedLibErrorMsg());
 		}
 
-		~BinaryPluginHandle() { if (handle) dlclose(handle); }
-		void *GetInfo() { return dlsym(handle, "plugin_info"); }
-		void *ResolveSymbol(const char* sym) { return dlsym(handle, sym); }
-		void *getHandle() { return handle; }
-	private:
-		PLUGIN_HANDLE handle; // pointer returned by dlopen on plugin shared lib
+		Logger::getLogger()->debug("%s - other plugin / RTLD_LAZY|RTLD_GLOBAL - name :%s: path :%s:", __FUNCTION__, name, path);
+	}
 
+	~BinaryPluginHandle()
+	{
+		if (handle)
+		{
+			SharedLibClose(handle);
+		}
+	}
+	void *GetInfo() { return SharedLibProcAddress(handle, "plugin_info"); }
+	void *ResolveSymbol(const char *sym) { return SharedLibProcAddress(handle, sym); }
+	void *getHandle() { return handle; }
+
+private:
+	PLUGIN_HANDLE handle; // pointer returned by SharedLibOpen on plugin shared lib
 };
 
 #endif
-
